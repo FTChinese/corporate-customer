f50e3b6aff9d3ebc3141fd98fe0c25acedebc6ff
neefrankie <neefrankie@gmail.com>
2020-03-16 20:58:20 +0800

    initial

---
+++ Makefile
@@ -0,0 +1,39 @@
+BUILD_DIR := build
+BINARY := corp-customer
+
+LINUX_BIN := $(BUILD_DIR)/linux/$(BINARY)
+
+VERSION := `git describe --tags`
+BUILD_AT := `date +%FT%T%z`
+LDFLAGS := -ldflags "-w -s -X main.version=${VERSION} -X main.build=${BUILD_AT}"
+
+.PHONY: build run publish linux restart config lastcommit clean test
+build :
+	go build $(LDFLAGS) -o $(BUILD_DIR)/$(BINARY) -v .
+
+run :
+	./$(BUILD_DIR)/$(BINARY)
+
+production :
+	./$(BUILD_DIR)/$(BINARY) -production
+
+deploy : linux
+	rsync -v $(LINUX_BIN) tk11:/home/node/go/bin/
+	ssh tk11 supervisorctl restart $(BINARY)
+
+linux :
+	GOOS=linux GOARCH=amd64 go build $(LDFLAGS) -o $(LINUX_BIN) -v .
+
+# Copy env varaible to server
+config :
+	rsync -v ../.env nodeserver:/home/node/go
+
+lastcommit :
+	git log --max-count=1 --pretty=format:%ad_%h --date=format:%Y_%m%d_%H%M
+
+clean :
+	go clean -x
+	rm -r build/*
+
+test :
+	echo $(BUILD)
--- main.go
+++ main.go
@@ -1,1 +1,91 @@
-package corp
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/FTChinese/go-rest/postoffice"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"gitlab.com/ftchinese/corp/database"
+	"net/http"
+	"os"
+)
+
+var (
+	isProduction bool
+	version      string
+	build        string
+	config       Config
+	logger       = logrus.WithField("project", "superyard").WithField("package", "main")
+)
+
+func init() {
+	flag.BoolVar(&isProduction, "production", false, "Indicate productions environment if present")
+	var v = flag.Bool("v", false, "print current version")
+
+	flag.Parse()
+
+	if *v {
+		fmt.Printf("%s\nBuild at %s\n", version, build)
+		os.Exit(0)
+	}
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+
+	viper.SetConfigName("api")
+	viper.AddConfigPath("$HOME/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	config = Config{
+		Debug:   !isProduction,
+		Version: version,
+		BuiltAt: build,
+		Year:    0,
+	}
+}
+
+func main() {
+	db, err := database.New(config.MustGetDBConn("mysql.master"))
+	if err != nil {
+		logger.Error(err)
+		os.Exit(1)
+	}
+
+	emailConn := MustGetEmailConn()
+	post := postoffice.NewPostman(
+		emailConn.Host,
+		emailConn.Port,
+		emailConn.User,
+		emailConn.Pass)
+
+	e := echo.New()
+	e.Renderer = MustNewRenderer(config)
+	e.HTTPErrorHandler = errorHandler
+
+	if !isProduction {
+		e.Static("/", "build/dev")
+	}
+
+	e.Use(middleware.Logger())
+	e.Use(session.Middleware(
+		sessions.NewCookieStore(
+			[]byte(MustGetSessionKey()),
+		),
+	))
+	e.Use(middleware.Recover())
+	//e.Use(middleware.CSRF())
+
+	e.GET("/", func(context echo.Context) error {
+		return context.Render(http.StatusOK, "base.html", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":3100"))
+}
