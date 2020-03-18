77b35f6036a9bc4e3b660c4e0def58ec4a3cbe0c
neefrankie <neefrankie@gmail.com>
2020-03-18 20:56:09 +0800

    feat: reader data

--- models/reader/profile.go
+++ models/reader/profile.go
@@ -1,1 +1,8 @@
 package reader
+
+// Profile contains the data of a reader belong to an organization.
+type Profile struct {
+	FtcID string `json:"ftcId" db:"ftc_id"`
+	Email string `json:"email" db:"email"`
+	Membership
+}
