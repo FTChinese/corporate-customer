de0209fba8d0fbd407a3f8105c613acdf17ae751
weiguo.ni <neefrankie@gmail.com>
2020-03-19 17:42:52 +0800

    feat: data type to build team from form

--- models/admin/team.go
+++ models/admin/team.go
@@ -3,7 +3,35 @@ package admin
 import (
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/guregu/null"
+	"strings"
 )
+
+type TeamForm struct {
+	Name         string `form:"name"`
+	InvoiceTitle string `form:"invoiceTitle"`
+	Errors       map[string]string
+}
+
+func (f *TeamForm) Sanitize() *TeamForm {
+	f.Name = strings.TrimSpace(f.Name)
+	f.InvoiceTitle = strings.TrimSpace(f.InvoiceTitle)
+
+	return f
+}
+
+func (f *TeamForm) Validate() bool {
+	return len(f.Errors) == 0
+}
+
+func (f *TeamForm) BuildTeam() Team {
+	return Team{
+		ID:           "",
+		Name:         f.Name,
+		InvoiceTitle: null.NewString(f.InvoiceTitle, f.InvoiceTitle != ""),
+		CreatedUTC:   chrono.TimeNow(),
+		UpdatedUTC:   chrono.TimeNow(),
+	}
+}
 
 type Team struct {
 	ID           string      `db:"team_id"`
