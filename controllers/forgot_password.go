06fc91817acb66998e0d135111f226db7daaa787
weiguo.ni <neefrankie@gmail.com>
2020-03-23 17:42:54 +0800

    feat: forgot password file

---
+++ controllers/forgot_password.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/labstack/echo/v4"
+
+func (router BarrierRouter) GetForgotPassword(c echo.Context) error {
+	return nil
+}
+
+func (router BarrierRouter) PostForgotPassword(c echo.Context) error {
+	return nil
+}
+
+func (router BarrierRouter) VerifyPasswordToken(c echo.Context) error {
+	return nil
+}
+
+func (router BarrierRouter) GetResetPassword(c echo.Context) error {
+	return nil
+}
+
+func (router BarrierRouter) PostResetPassword(c echo.Context) error {
+	return nil
+}
