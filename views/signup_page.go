fb490c3a02a5fa9eb3f850e9fe87b9524633f4ab
weiguo.ni <neefrankie@gmail.com>
2020-03-23 17:42:34 +0800

    feat: build signup page

---
+++ views/signup_page.go
@@ -0,0 +1,46 @@
+package views
+
+import (
+	"github.com/FTChinese/b2b/models/admin"
+	"github.com/FTChinese/b2b/widget"
+)
+
+func NewSignUpForm(value admin.SignUp) widget.Form {
+	return widget.Form{
+		Disabled: false,
+		Action:   "",
+		Fields: []widget.FormControl{
+			{
+				Label:       "邮箱",
+				ID:          "email",
+				Type:        widget.ControlTypeEmail,
+				Name:        "email",
+				Value:       value.Email,
+				Placeholder: "admin@example.org",
+				Required:    true,
+			},
+			{
+				Label:    "密码",
+				ID:       "password",
+				Type:     widget.ControlTypePassword,
+				Name:     "password",
+				Value:    "",
+				Required: true,
+			},
+			{
+				Label:    "确认密码",
+				ID:       "confirmPassword",
+				Type:     widget.ControlTypePassword,
+				Name:     "confirmPassword",
+				Value:    "",
+				Required: true,
+			},
+		},
+		SubmitBtn: widget.Button{
+			DisableWith: "正在创建...",
+			Text:        "创建账号",
+		},
+		CancelBtn: widget.Link{},
+		DeleteBtn: widget.Link{},
+	}
+}
