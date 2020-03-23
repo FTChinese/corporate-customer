8f0f037ee4553b2bbc7a4d1ad36f3faee4870ce0
weiguo.ni <neefrankie@gmail.com>
2020-03-23 17:44:34 +0800

    refactor: using CtxBuilder

---
+++ controllers/sign_in.go
@@ -0,0 +1,73 @@
+package controllers
+
+import (
+	"github.com/FTChinese/b2b/models/admin"
+	"github.com/FTChinese/b2b/views"
+	"github.com/FTChinese/go-rest/postoffice"
+	"github.com/gorilla/sessions"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type BarrierRouter struct {
+	db   *sqlx.DB
+	post postoffice.Postman
+}
+
+func NewBarrierRouter(db *sqlx.DB, p postoffice.Postman) BarrierRouter {
+	return BarrierRouter{db: db, post: p}
+}
+
+func (router BarrierRouter) GetLogin(c echo.Context) error {
+	ctx := views.NewCtxBuilder().
+		WithForm(views.NewLoginForm(admin.Login{})).
+		Build()
+
+	return c.Render(http.StatusOK, "login.html", ctx)
+}
+
+func createSession(c echo.Context) *sessions.Session {
+	sess, _ := session.Get(sessionKey, c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   86400 * 7,
+		HttpOnly: false,
+	}
+
+	return sess
+}
+
+func (router BarrierRouter) PostLogin(c echo.Context) error {
+	var l admin.Login
+	if err := c.Bind(&l); err != nil {
+		return err
+	}
+
+	if ok := l.Sanitize().Validate(); !ok {
+		ctx := views.NewCtxBuilder().
+			WithForm(views.NewLoginForm(l)).
+			Build()
+
+		return c.Render(http.StatusOK, "login.html", ctx)
+	}
+
+	sess := createSession(c)
+	sess.Values[loggedInKey] = l.Email
+	sess.Save(c.Request(), c.Response())
+
+	return c.Redirect(http.StatusFound, SiteMap.Home)
+}
+
+func (router BarrierRouter) LogOut(c echo.Context) error {
+	sess, _ := session.Get(sessionKey, c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   -1, // A zero or negative number will expire the cookie immediately. If both Expires and Max-Age are set, Max-Age has precedence.
+		HttpOnly: false,
+	}
+	sess.Save(c.Request(), c.Response())
+
+	return c.Redirect(http.StatusFound, SiteMap.Login)
+}
