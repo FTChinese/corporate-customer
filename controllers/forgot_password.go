11cf0e5037e6e80b11be4c6cfa77581ece4028da
neefrankie <neefrankie@gmail.com>
2020-03-24 14:23:30 +0800

    feat: enter email to reset password

--- controllers/forgot_password.go
+++ controllers/forgot_password.go
@@ -1,13 +1,40 @@
 package controllers
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/FTChinese/b2b/models/admin"
+	"github.com/FTChinese/b2b/views"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
 
+// GetForgotPassword show a form to collection user's email
 func (router BarrierRouter) GetForgotPassword(c echo.Context) error {
-	return nil
+	ctx := views.NewCtxBuilder().
+		WithForm(views.NewResetLetterForm(admin.Identity{})).
+		Build()
+	return c.Render(http.StatusOK, "password_reset_email.html", ctx)
 }
 
+// PostForgotPassword handles sending email to help reset password.
 func (router BarrierRouter) PostForgotPassword(c echo.Context) error {
-	return nil
+	var i admin.Identity
+	if err := c.Bind(&i); err != nil {
+		return err
+	}
+
+	i.Sanitize()
+
+	if ok := i.Validate(); !ok {
+		ctx := views.NewCtxBuilder().
+			WithForm(views.NewResetLetterForm(i)).
+			Build()
+
+		return c.Render(http.StatusOK, "password_reset_email.html", ctx)
+	}
+
+	ctx := views.NewCtxBuilder().Set("done", true).Build()
+
+	return c.Render(http.StatusOK, "password_reset_email.html", ctx)
 }
 
 func (router BarrierRouter) VerifyPasswordToken(c echo.Context) error {
