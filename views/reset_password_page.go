b8e8b6c07b6902486c32efccf65e08249d3fb78f
neefrankie <neefrankie@gmail.com>
2020-03-24 14:18:43 +0800

    feat: add password reset page

---
+++ views/reset_password_page.go
@@ -0,0 +1,30 @@
+package views
+
+import (
+	"github.com/FTChinese/b2b/models/admin"
+	"github.com/FTChinese/b2b/widget"
+)
+
+func NewResetLetterForm(i admin.Identity) widget.Form {
+	return widget.Form{
+		Disabled: false,
+		Method:   widget.MethodPost,
+		Action:   "",
+		Fields: []widget.FormControl{
+			{
+				Label:       "邮箱",
+				ID:          "email",
+				Type:        widget.ControlTypeEmail,
+				Name:        "email",
+				Value:       i.Email,
+				Placeholder: "admin@example.org",
+				Required:    true,
+			},
+		},
+		SubmitBtn: widget.PrimaryBlockBtn.
+			SetName("发送邮件").
+			SetDisabledText("正在发送..."),
+		CancelBtn: widget.Link{},
+		DeleteBtn: widget.Link{},
+	}
+}
