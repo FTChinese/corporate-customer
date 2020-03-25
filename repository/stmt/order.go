6a31f41d1401832b0ef0fbd2fb524cd809512289
weiguo.ni <neefrankie@gmail.com>
2020-03-25 17:37:25 +0800

    feat: sql for order

--- repository/stmt/order.go
+++ repository/stmt/order.go
@@ -1,1 +1,13 @@
 package stmt
+
+const CreateOrder = `
+INSERT into b2b.transaction
+SET id = :tnx_id,
+	price = :price,
+	amount = :amount,
+	tier = :tier,
+	cycle = :cycle,
+	cycle_count = :cycle_count,
+	trial_days = :trial_days,
+	kind = :kind,
+	created_utc = UTC_TIMESTAMP()`
