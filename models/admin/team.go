752eff09e2aea644e85bf729e036d75a93aea5be
weiguo.ni <neefrankie@gmail.com>
2020-03-31 11:11:23 +0800

    feat: validate new team data, comparison to updating values and update

--- models/admin/team.go
+++ models/admin/team.go
@@ -1,37 +1,35 @@
 package admin
 
 import (
+	"github.com/FTChinese/b2b/models/validator"
 	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/rand"
 	"github.com/guregu/null"
 	"strings"
 )
 
 // TeamForm is the form data submitted to create a Team.
 type TeamForm struct {
-	Name         string `form:"name"`
-	InvoiceTitle string `form:"invoiceTitle"`
+	Name         string `form:"name"`         // Organization name. This is used for display only. Required. 128 chars.
+	InvoiceTitle string `form:"invoiceTitle"` // Invoice title might be different from Name. Optional.
 	Errors       map[string]string
 }
 
-func (f *TeamForm) Sanitize() *TeamForm {
+func (f *TeamForm) Validate() bool {
 	f.Name = strings.TrimSpace(f.Name)
 	f.InvoiceTitle = strings.TrimSpace(f.InvoiceTitle)
 
-	return f
-}
-
-func (f *TeamForm) Validate() bool {
-	return len(f.Errors) == 0
-}
-
-func (f *TeamForm) BuildTeam() Team {
-	return Team{
-		ID:           "",
-		Name:         f.Name,
-		InvoiceTitle: null.NewString(f.InvoiceTitle, f.InvoiceTitle != ""),
-		CreatedUTC:   chrono.TimeNow(),
-		UpdatedUTC:   chrono.TimeNow(),
+	msg := validator.New("机构名称").Required().Max(128).Validate(f.Name)
+	if msg != "" {
+		f.Errors["name"] = msg
 	}
+
+	msg = validator.New("发票抬头").Max(512).Validate(f.InvoiceTitle)
+	if msg != "" {
+		f.Errors["invoiceTitle"] = msg
+	}
+
+	return len(f.Errors) == 0
 }
 
 // Team represents an existing b2b entity.
@@ -43,5 +41,29 @@ type Team struct {
 	InvoiceTitle null.String `db:"invoice_title"`
 	CreatedUTC   chrono.Time `db:"created_utc"`
 	UpdatedUTC   chrono.Time `db:"updated_utc"`
-	Admin        string      `db:"admin"`
+	AdminID      string      `db:"admin_id"`
+}
+
+func NewTeam(f TeamForm, adminID string) Team {
+	return Team{
+		ID:           "team_" + rand.String(12),
+		Name:         f.Name,
+		InvoiceTitle: null.NewString(f.InvoiceTitle, f.InvoiceTitle != ""),
+		CreatedUTC:   chrono.TimeNow(),
+		UpdatedUTC:   chrono.TimeNow(),
+		AdminID:      adminID,
+	}
+}
+
+// IsEqual compares the original values of team and
+// updated values. If they are equal, no further operation
+// should be conducted.
+func (t Team) IsEqual(f TeamForm) bool {
+	return t.Name == f.Name && t.InvoiceTitle.String == f.InvoiceTitle
+}
+
+// Update updates the current team to new values.
+func (t *Team) Update(f TeamForm) {
+	t.Name = f.Name
+	t.InvoiceTitle = null.NewString(f.InvoiceTitle, f.InvoiceTitle != "")
 }
