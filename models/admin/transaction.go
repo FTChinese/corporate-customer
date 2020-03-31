a99f76b8dd96127cd2b2a00f55a7987a0bec5c31
weiguo.ni <neefrankie@gmail.com>
2020-03-31 17:41:41 +0800

    refactor: rename to Order

--- models/admin/transaction.go
+++ models/admin/transaction.go
@@ -11,7 +11,7 @@ type Cart struct {
 	Quantity int64  `form:"quantity"` // How many copies of licence for this plan
 }
 
-// Transaction describes the details of each transaction
+// Order describes the details of each transaction
 // to purchase a licence.
 // If a transaction is used to purchase a new licence, the
 // licence should be created together with the order but marked
@@ -23,7 +23,7 @@ type Cart struct {
 // it is confirmed, which will result licence extended or
 // upgraded and the membership (if the licence is granted
 // to someone) will be backed up and updated corresponding.
-type Transaction struct {
+type Order struct {
 	ID           string         `db:"order_id"`
 	PlanID       string         `db:"plan_id"`
 	LicenceID    string         `db:"licence_id"`
