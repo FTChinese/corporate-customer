3087a0bf7544113150b0633e83aa8090d883ec4a
weiguo.ni <neefrankie@gmail.com>
2020-04-02 18:01:50 +0800

    removed

--- models/reader/profile.go
+++
@@ -1,8 +0,0 @@
-package reader
-
-// Profile contains the data of a reader belong to an organization.
-type Profile struct {
-	FtcID string `json:"ftcId" db:"ftc_id"`
-	Email string `json:"email" db:"email"`
-	Membership
-}
