a61a5f16b3d17c82bed751376c2afedb749712f9
weiguo.ni <neefrankie@gmail.com>
2020-04-02 17:58:48 +0800

    refactor: add TeamID, PlanID, Active fields

--- models/admin/licence.go
+++ models/admin/licence.go
@@ -1,13 +1,17 @@
 package admin
 
 import (
+	"github.com/FTChinese/b2b/models/plan"
 	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/go-rest/enum"
 	"github.com/guregu/null"
 )
 
-// Licence is the subscription a team purchased.
-// A team can purchase multiple licences, as they often do.
+// Licence is the licence a team purchased.
+// Any licence must have a plan attached to it,
+// and it must belong to a term.
+// Team -> Licence and Plan -> Licence are both
+// one-to-many relations.
+// A team can purchase multiple licences, as they usually do.
 // After purchasing licences, the team can send
 // invitations to team member for a specific licence.
 // Once a team member accepts the invitation, this licence
@@ -18,11 +22,16 @@ import (
 // the AssigneeID field.
 type Licence struct {
 	ID         string      `db:"licence_id"`
-	Tier       enum.Tier   `db:"tier"`
-	Cycle      enum.Cycle  `db:"cycle"`
+	TeamID     string      `db:"team_id"`
+	PlanID     string      `db:"plan_id"`
 	ExpireDate chrono.Date `db:"expire_date"`
 	AssigneeID null.String `db:"assignee_id"`
+	Active     bool        `db:"is_active"`
 	CreatedUTC chrono.Time `db:"created_utc"`
 	UpdatedUTC chrono.Time `db:"updated_utc"`
-	Team       string      `db:"team"`
+	Plan       plan.BasePlan
+}
+
+func (l Licence) IsAvailable() bool {
+	return l.AssigneeID.IsZero()
 }
