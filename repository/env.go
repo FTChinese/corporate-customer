ba1553052c8d2c0fe88fa4fd6207c0108f8f63e0
neefrankie <neefrankie@gmail.com>
2020-04-03 21:34:21 +0800

    feat: beginGrantTx

--- repository/env.go
+++ repository/env.go
@@ -10,3 +10,13 @@ var logger = logrus.WithField("package", "repository")
 type Env struct {
 	db *sqlx.DB
 }
+
+func (env Env) beginGrantTx() (GrantTx, error) {
+	tx, err := env.db.Beginx()
+
+	if err != nil {
+		return GrantTx{}, err
+	}
+
+	return GrantTx{tx}, nil
+}
