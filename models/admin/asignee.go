b4af3de76f29718d80d88a1f586adc7138d778f6
neefrankie <neefrankie@gmail.com>
2020-04-09 21:52:43 +0800

    feat: Assignee

---
+++ models/admin/asignee.go
@@ -0,0 +1,13 @@
+package admin
+
+import "github.com/guregu/null"
+
+// Assignee represents a reader who can accept
+// an invitation email, and who can be granted
+// a licence.
+type Assignee struct {
+	Email    null.String `db:"email"`
+	FtcID    null.String `db:"ftc_id"`
+	UserName null.String `db:"user_name"`
+	IsVIP    bool        `db:"is_vip"`
+}
