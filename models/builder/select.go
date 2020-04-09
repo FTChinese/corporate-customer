4e2c4fb96e67f3d2af2ea71a9f25c87b7a6e1f3d
neefrankie <neefrankie@gmail.com>
2020-04-09 21:56:36 +0800

    feat: sql query builder

---
+++ models/builder/select.go
@@ -0,0 +1,104 @@
+package builder
+
+import (
+	"strconv"
+	"strings"
+)
+
+type Select struct {
+	cols    []string
+	from    string
+	where   string
+	orderBy string
+	limit   int
+	paged   bool
+	lock    bool
+}
+
+func NewSelect() Select {
+	return Select{
+		cols:    make([]string, 0),
+		from:    "",
+		orderBy: "",
+		limit:   0,
+		paged:   false,
+	}
+}
+
+func (s Select) AddColumn(col string) Select {
+	s.cols = append(s.cols, col)
+	return s
+}
+
+func (s Select) From(from string) Select {
+	s.from = from
+	return s
+}
+
+func (s Select) Where(w string) Select {
+	s.where = w
+	return s
+}
+
+func (s Select) OrderBy(o string) Select {
+	s.orderBy = o
+	return s
+}
+
+func (s Select) Limit(rows int) Select {
+	s.limit = rows
+	return s
+}
+
+func (s Select) Paged() Select {
+	s.paged = true
+	return s
+}
+
+func (s Select) Lock() Select {
+	s.lock = true
+	return s
+}
+
+// Build produces a SQL SELECT statement:
+// SELECT ...
+// FROM ...
+// WHERE ...
+// ORDER BY ...
+// LIMIT ? [OFFSET ?]
+func (s Select) Build() string {
+	var buf strings.Builder
+	buf.WriteString("SELECT ")
+	buf.WriteString(strings.Join(s.cols, ","))
+	buf.WriteByte(' ')
+	buf.WriteString("FROM ")
+	buf.WriteString(s.from)
+	if s.where != "" {
+		buf.WriteByte(' ')
+		buf.WriteString("WHERE ")
+		buf.WriteString(s.where)
+	}
+
+	if s.orderBy != "" {
+		buf.WriteByte(' ')
+		buf.WriteString("ORDER BY ")
+		buf.WriteString(s.orderBy)
+	}
+
+	if s.limit > 0 {
+		buf.WriteByte(' ')
+		buf.WriteString("LIMIT ")
+		buf.WriteString(strconv.Itoa(s.limit))
+
+	} else if s.paged {
+		buf.WriteByte(' ')
+		buf.WriteString("LIMIT ? OFFSET ?")
+	}
+
+	if s.lock {
+		buf.WriteByte(' ')
+		buf.WriteString("FOR UPDATE")
+	}
+
+	return buf.String()
+}
