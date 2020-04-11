5b912d85e20fdfa3a7593f72b73886c779dc766e
neefrankie <neefrankie@gmail.com>
2020-04-12 00:15:07 +0800

    refactor: add json tag

--- models/admin/account.go
+++ models/admin/account.go
@@ -2,19 +2,95 @@ package admin
 
 import (
 	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/postoffice"
 	"github.com/guregu/null"
+	"strings"
+	"text/template"
 )
 
 // Account is an organization's administrator account.
 // An account might manage multiple teams/organizations.
 // Currently we allow only one team per account.
 type Account struct {
-	ID          string      `db:"admin_id"`
-	Email       string      `db:"email"`
-	DisplayName null.String `db:"display_name"`
-	Active      bool        `db:"active"`
-	Verified    bool        `db:"verified"`
-	CreatedUTC  chrono.Time `db:"created_utc"`
-	UpdatedUTC  chrono.Time `db:"updated_utc"`
-	Team        null.String `db:"team"` // The id of the team associated with this account.
+	ID          string      `json:"id" db:"admin_id"`
+	Email       string      `json:"email" db:"email"`
+	DisplayName null.String `json:"displayName" db:"display_name"`
+	Active      bool        `json:"active" db:"active"`
+	Verified    bool        `json:"verified" db:"verified"`
+}
+
+func (a Account) NormalizeName() string {
+	if a.DisplayName.Valid {
+		return a.DisplayName.String
+	}
+
+	return strings.Split(a.Email, "@")[0]
+}
+
+func (a Account) VerificationLetter(letter Letter) (postoffice.Parcel, error) {
+	tmpl, err := template.New("passwordReset").Parse(letterVerification)
+
+	if err != nil {
+		return postoffice.Parcel{}, err
+	}
+
+	data := struct {
+		Name string
+		Letter
+	}{
+		Name:   a.NormalizeName(),
+		Letter: letter,
+	}
+	var body strings.Builder
+	err = tmpl.Execute(&body, data)
+
+	if err != nil {
+		return postoffice.Parcel{}, err
+	}
+
+	return postoffice.Parcel{
+		FromAddress: "no-reply@ftchinese.com",
+		FromName:    "FT中文网",
+		ToAddress:   a.Email,
+		ToName:      a.NormalizeName(),
+		Subject:     "[FT中文网B2B]验证账号",
+		Body:        body.String(),
+	}, nil
+}
+
+func (a Account) PasswordResetLetter(letter Letter) (postoffice.Parcel, error) {
+	tmpl, err := template.New("passwordReset").Parse(letterPasswordReset)
+
+	if err != nil {
+		return postoffice.Parcel{}, err
+	}
+
+	data := struct {
+		Name string
+		Letter
+	}{
+		Name:   a.NormalizeName(),
+		Letter: letter,
+	}
+	var body strings.Builder
+	err = tmpl.Execute(&body, data)
+
+	if err != nil {
+		return postoffice.Parcel{}, err
+	}
+
+	return postoffice.Parcel{
+		FromAddress: "no-reply@ftchinese.com",
+		FromName:    "FT中文网",
+		ToAddress:   a.Email,
+		ToName:      data.Name,
+		Subject:     "[FT中文网B2B]重置密码",
+		Body:        body.String(),
+	}, nil
+}
+
+type Profile struct {
+	Account
+	CreatedUTC chrono.Time `db:"created_utc"`
+	UpdatedUTC chrono.Time `db:"updated_utc"`
 }
