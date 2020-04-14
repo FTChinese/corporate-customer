9b6660f91152ba0dc09e71f5ccd33c54b52852d7
weiguo.ni <neefrankie@gmail.com>
2020-04-14 17:26:31 +0800

    feat: build insert order stmt

--- repository/stmt/stmt_order.go
+++ repository/stmt/stmt_order.go
@@ -1,5 +1,38 @@
 package stmt
 
+import "github.com/FTChinese/b2b/models/sq"
+
+const CreateOrder = `
+INSERT INTO b2b.order (
+	id,
+	plan_id,
+	discount_id,
+	licence_id,
+	team_id,
+	checkout_id,
+	amount,
+	cycle_count,
+	trial_days,
+	kind,
+	created_utc
+) VALUES `
+
+var OrderBuilder = sq.NewInsert().
+	Into(sq.NewTable("b2b.order")).
+	SetColumns([]sq.Column{
+		sq.NewColumn("id"),
+		sq.NewColumn("plan_id"),
+		sq.NewColumn("discount_id"),
+		sq.NewColumn("licence_id"),
+		sq.NewColumn("team_id"),
+		sq.NewColumn("checkout_id"),
+		sq.NewColumn("amount"),
+		sq.NewColumn("cycle_count"),
+		sq.NewColumn("trial_days"),
+		sq.NewColumn("kind"),
+		sq.NewColumn("created_utc"),
+	})
+
 const Order = `
 SELECT id AS order_id,
 	plan_id,
