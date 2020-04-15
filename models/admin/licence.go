04927ac4d1bdb47bcc36df3f95ae8a846cd331ef
neefrankie <neefrankie@gmail.com>
2020-04-15 23:35:03 +0800

    refactor: changed sql schema with current_plan and last_invitation columns

--- models/admin/licence.go
+++ models/admin/licence.go
@@ -1,16 +1,13 @@
 package admin
 
 import (
+	"encoding/json"
 	"github.com/FTChinese/b2b/models/plan"
 	"github.com/FTChinese/go-rest/chrono"
 	"github.com/guregu/null"
 )
 
-// Licence is the licence a team purchased.
-// Any licence must have a plan attached to it,
-// and it must belong to a term.
-// Team -> Licence and Plan -> Licence are both
-// one-to-many relations.
+// BaseLicence is the licence a team purchased.
 // A team can purchase multiple licences, as they usually do.
 // After purchasing licences, the team can send
 // invitations to team member for a specific licence.
@@ -20,44 +17,69 @@ import (
 // When a licence is revoked, backup reader's current
 // current membership status and delete that row, clear
 // the AssigneeID field.
-type Licence struct {
-	ID         string      `db:"licence_id"`
-	TeamID     string      `db:"team_id"`
-	AssigneeID null.String `db:"assignee_id"` // Only exists after reader accepted an invitation.
-	ExpireDate chrono.Date `db:"expire_date"`
-	Active     bool        `db:"is_active"` // Only active after payment received. This is not controlled by the admin.
-	CreatedUTC chrono.Time `db:"created_utc"`
-	UpdatedUTC chrono.Time `db:"updated_utc"`
+type BaseLicence struct {
+	ID         string      `json:"id" db:"licence_id"`
+	TeamID     string      `json:"team_id" db:"team_id"`
+	AssigneeID null.String `json:"-" db:"assignee_id"` // Only exists after reader accepted an invitation.
+	ExpireDate chrono.Date `json:"expireDate" db:"expire_date"`
+	Status     null.String `json:"status" db:"current_status"`
+	CreatedUTC chrono.Time `json:"createdUtc" db:"created_utc"`
+	UpdatedUTC chrono.Time `json:"updatedUtc" db:"updated_utc"`
 }
 
 // IsAvailable checks whether the licence is
 // assigned to someone else.
-func (l Licence) IsAvailable() bool {
-	return l.AssigneeID.IsZero() && l.Active
+func (l BaseLicence) IsAvailable() bool {
+	return l.AssigneeID.IsZero()
 }
 
-func (l *Licence) AssignTo(ftcID string) {
+func (l *BaseLicence) AssignTo(ftcID string) {
 	l.AssigneeID = null.StringFrom(ftcID)
 }
 
-// ExpandedLicence includes the plan of this licence
-// and optional assignee.
+// ExpandedLicence includes the data of the licence,
+// its attached plan, a latest invitation if present
+// and the assignee if it is already granted.
 type ExpandedLicence struct {
-	Licence
-	Plan     plan.BasePlan
-	Assignee Assignee
+	BaseLicence
+	Plan       plan.BasePlan
+	Invitation Invitation // If no one is invited to use this licence, its fields are empty.
+	Assignee   Assignee   // If no use is granted to use this licence, its fields are empty.
 }
 
+// LicenceList is used to output a list of licence with pagination.
+type LicenceList struct {
+	Total int64             `json:"total"` // The total number of rows.
+	Data  []ExpandedLicence `json:"data"`
+}
+
+// LicenceSchema is used to save/retrieve licence.
 type LicenceSchema struct {
-	Licence
-	plan.BasePlan
+	BaseLicence
+	CurrentPlan    string      `db:"current_plan"`    // The raw JSON column is a string.
+	LastInvitation null.String `db:"last_invitation"` // Raw json column.
 	Assignee
 }
 
-func (s LicenceSchema) ExpandedLicence() ExpandedLicence {
-	return ExpandedLicence{
-		Licence:  s.Licence,
-		Plan:     s.BasePlan,
-		Assignee: s.Assignee,
+// ExpandedLicence transforms a row retrieved from DB to output format.
+func (s LicenceSchema) ExpandedLicence() (ExpandedLicence, error) {
+	var p plan.BasePlan
+	var inv Invitation
+
+	if err := json.Unmarshal([]byte(s.CurrentPlan), &p); err != nil {
+		return ExpandedLicence{}, err
 	}
+
+	if s.LastInvitation.Valid {
+		if err := json.Unmarshal([]byte(s.LastInvitation.String), &inv); err != nil {
+			return ExpandedLicence{}, err
+		}
+	}
+
+	return ExpandedLicence{
+		BaseLicence: s.BaseLicence,
+		Plan:        p,
+		Invitation:  inv,
+		Assignee:    s.Assignee,
+	}, nil
 }
