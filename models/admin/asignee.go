e6ac1f03431fb44948f24ebbab7b54fccc4adb03
neefrankie <neefrankie@gmail.com>
2020-04-15 23:39:09 +0800

    refactor: add json tag

--- models/admin/asignee.go
+++ models/admin/asignee.go
@@ -1,15 +1,28 @@
 package admin
 
-import "github.com/guregu/null"
+import (
+	"github.com/guregu/null"
+	"strings"
+)
 
 // Assignee represents a reader who can accept
 // an invitation email, and who can be granted
 // a licence.
 type Assignee struct {
-	Email    null.String `db:"email"`
-	FtcID    null.String `db:"ftc_id"`
-	UserName null.String `db:"user_name"`
-	IsVIP    bool        `db:"is_vip"`
+	FtcID    null.String `json:"ftcId" db:"ftc_id"`
+	Email    null.String `json:"email" db:"email"`
+	UserName null.String `json:"userName" db:"user_name"`
+	IsVIP    bool        `json:"isVip" db:"is_vip"`
+}
+
+// NormalizeName tries to find a proper way to greet user
+// in email.
+func (a Assignee) NormalizeName() string {
+	if a.UserName.Valid {
+		return a.UserName.String
+	}
+
+	return strings.Split(a.Email.String, "@")[0]
 }
 
 type AssigneeSchema struct {
