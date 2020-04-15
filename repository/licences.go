1db27c03710f03dc4865207824ca14fb91c14be7
neefrankie <neefrankie@gmail.com>
2020-04-15 23:41:03 +0800

    refactor: remove CreateLicence

--- repository/licences.go
+++ repository/licences.go
@@ -5,24 +5,10 @@ import (
 	"github.com/FTChinese/b2b/repository/stmt"
 )
 
-const stmtCreateLicence = `
-INSERT INTO b2b.licence
-SET id = :licence_id
-	team_id = :team_id,
-	plan_id = :plan_id,
-	created_utc = UTC_TIMESTAMP()`
-
-func (env Env) CreateLicence(l admin.Licence) error {
-	_, err := env.db.NamedExec(stmtCreateLicence, l)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// LoadLicence retrieves an expanded licence
-// belong to a team.
+// LoadLicence retrieves a licence, together with its
+// subscription plan and the user to whom it was assigned.
+// If the licence is not assigned yet, user part should
+// have empty value.
 func (env Env) LoadLicence(id, teamID string) (admin.ExpandedLicence, error) {
 	var ls admin.LicenceSchema
 	err := env.db.Get(&ls, stmt.ExpandedLicence, id, teamID)
@@ -31,7 +17,7 @@ func (env Env) LoadLicence(id, teamID string) (admin.ExpandedLicence, error) {
 		return admin.ExpandedLicence{}, err
 	}
 
-	return ls.ExpandedLicence(), nil
+	return ls.ExpandedLicence()
 }
 
 func (env Env) ListLicence(teamID string) ([]admin.ExpandedLicence, error) {
@@ -45,7 +31,11 @@ func (env Env) ListLicence(teamID string) ([]admin.ExpandedLicence, error) {
 
 	el := make([]admin.ExpandedLicence, 0)
 	for _, v := range ls {
-		el = append(el, v.ExpandedLicence())
+		l, err := v.ExpandedLicence()
+		if err != nil {
+			return nil, err
+		}
+		el = append(el, l)
 	}
 	return el, nil
 }
