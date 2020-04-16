e0557c41bb26f89602e1490799126866c097baf0
neefrankie <neefrankie@gmail.com>
2020-04-16 21:59:03 +0800

    refactor: composing invitation letter

--- models/admin/letter_composer.go
+++ models/admin/letter_composer.go
@@ -1,6 +1,12 @@
 package admin
 
-import "github.com/FTChinese/go-rest/enum"
+import (
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/FTChinese/go-rest/postoffice"
+	"strings"
+)
+
+const baseUrl = "https://www.ftacademy.cn/b2b"
 
 // Letter is the data passed to template to generate the content
 // of an email.
@@ -14,4 +20,36 @@ type InvitationLetter struct {
 	TeamName   string
 	Tier       enum.Tier
 	URL        string
+}
+
+func ComposeInvitationLetter(a Assignee, l Licence, at AccountTeam) (postoffice.Parcel, error) {
+	data := struct {
+		AssigneeName string
+		TeamName     string
+		Tier         enum.Tier
+		URL          string
+		AdminEmail   string
+	}{
+		AssigneeName: a.NormalizeName(),
+		TeamName:     at.TeamName.String,
+		Tier:         l.Plan.Tier,
+		URL:          baseUrl + "/accept-invitation/" + l.Invitation.Token,
+		AdminEmail:   at.Email,
+	}
+
+	var body strings.Builder
+	err := tmpl.ExecuteTemplate(&body, "invitation", data)
+
+	if err != nil {
+		return postoffice.Parcel{}, err
+	}
+
+	return postoffice.Parcel{
+		FromAddress: "no-reply@ftchinese.com",
+		FromName:    "FT中文网",
+		ToAddress:   a.Email.String,
+		ToName:      data.AssigneeName,
+		Subject:     "[FT中文网B2B]会员邀请",
+		Body:        body.String(),
+	}, nil
 }
