89792fb22ed2537b558d25e2385c9a6db87ae2ed
weiguo.ni <neefrankie@gmail.com>
2020-04-16 17:32:38 +0800

    refactor: add stmt SignUp

--- repository/stmt/admin_account.go
+++ repository/stmt/admin_account.go
@@ -2,6 +2,15 @@
 // Most of them cannot be executed alone.
 package stmt
 
+const SignUp = `
+INSERT INTO b2b.admin
+SET id = :admin_id,
+	email = :email,
+	password_sha2 = UNHEX(SHA2(:password, 256)),
+	vrf_token = UNHEX(:token),
+	created_utc = UTC_TIMESTAMP(),
+	updated_utc = UTC_TIMESTAMP()`
+
 const accountBase = `
 SELECT a.id AS admin_id,
 	a.team_id AS team_id,
@@ -10,30 +19,56 @@ SELECT a.id AS admin_id,
 	a.is_active AS active,
 	a.verified AS verified`
 
-const selectAccount = accountBase + `
-FROM b2b.admin AS a`
-
-const selectJWTAccount = accountBase + `,
-	t.team_id AS team_id
+// selectAccountTeam retrieves admin's account
+// and the team linked to it.
+const selectAccountTeam = accountBase + `,
+	t.team_id AS team_id,
+	t.name AS team_name
 FROM b2b.admin AS a
 	LEFT JOIN b2b.team AS t
 	ON a.id = t.admin_id`
 
-const JWTAccount = selectJWTAccount + `
+// AccountTeam retrieves Account + Team
+// by account id.
+const AccountTeam = selectAccountTeam + `
 WHERE a.id = ?
 LIMIT 1`
 
-const Login = selectJWTAccount + `
+// Login retrieves Account + Team by comparing
+// credentials.
+const Login = selectAccountTeam + `
 WHERE (a.email, a.password_sha2) = (?, UNHEX(SHA2(?, 256)))
 LIMIT 1`
+
+// selectAccount manipulate admin's account itself, without
+// team data.
+const selectAccount = accountBase + `
+FROM b2b.admin AS a`
 
 const AccountByID = selectAccount + `,
 WHERE a.id = ?
 LIMIT 1`
 
+// AccountByVerifier retrieves admin account by
+// verification token.
 const AccountByVerifier = selectAccount + `
 WHERE a.vrf_token = UNHEX(?)
 LIMIT 1`
+
+// EmailVerified set the verified column to true
+// for a verification token.
+const EmailVerified = `
+UPDATE b2b.admin
+	SET verified = 1
+WHERE id = :admin_id
+LIMIT 1`
+
+const ReGenerateVrfToken = `
+UPDATE b2b.admin
+SET token = UNHEX(:token),
+	updated_utc = UTC_TIMESTAMP()
+WHERE id = :admin_id
+LIMIT1`
 
 // AccountByEmail retrieves an admin's account
 // by email column.
@@ -62,4 +97,18 @@ const PasswordMatched = `
 SELECT password_sha2 = UNHEX(SHA2(?, 256)) AS matched
 FROM b2b.admin
 WHERE id = ?
+LIMIT 1`
+
+const UpdateName = `
+UPDATE b2b.admin
+SET display_name = :display_name,
+	updated_utc = UTC_TIMESTAMP()
+WHERE id = :admin_id
+LIMIT 1`
+
+const UpdatePassword = `
+UPDATE b2b.admin
+SET password_sha2 = UNHEX(SHA2(:password, 256))
+	updated_utc = UTC_TIMESTAMP()
+WHERE id = :amin_id
 LIMIT 1`
