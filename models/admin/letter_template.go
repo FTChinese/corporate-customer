44d1d24cd925fe909770f29399c66ab21cbd7476
neefrankie <neefrankie@gmail.com>
2020-04-19 21:58:33 +0800

    feat: letter after licence granted

--- models/admin/letter_template.go
+++ models/admin/letter_template.go
@@ -6,7 +6,7 @@ import (
 
 const letterTemplates = `
 {{define "verification"}}
-FT中文网B2B用户 {{.Name}}，你好！
+FT中文网B2B管理员 {{.Name}}，你好！
 
 {{if SignUp}}
 感谢您注册FT中文网B2B订阅服务。您在此可以为您所属机构的成员订阅FT付费服务。请注意，B2B服务的账号独立于FT中文网的账号，如果您未使用此邮箱注册FT中文网账号，则此邮箱只能登录B2B服务。
@@ -36,6 +36,8 @@ FT中文网B2B用户 {{.Name}}，你好！
 
 本链接3小时内有效。
 
+本邮件由系统自动生成，请勿回复。
+
 FT中文网
 {{end}}
 
@@ -50,7 +52,25 @@ FT中文网读者 {{.AssigneeName}}，你好！
 
 本链接3日内有效，请尽快接受邀请。如果链接已过期，请联系您所属机构的管理员 {{.AdminEmail}}。
 
+本邮件由系统自动生成，请勿回复。
+
 FT中文网
-{{end}}`
+{{end}}
+
+{{define "licenceGranted"}}
+FT中文网B2B管理员 {{.Name}}，你好！
+
+您通过FT中文网B2B业务邀请团队成员{{.AssigneeEmail}}成为FT中文网订阅用户，该成员已经接受了邀请，订阅方案的许可已经授予该用户：
+
+订阅方案：{{.Tier.StringCN}}
+到期日期：{{.ExpirationDate.String}}
+
+您随时可以在B2B管理系统中撤销该用户的许可。
+
+本邮件由系统自动生成，请勿回复。
+
+FT中文网
+{{end}}
+`
 
 var tmpl = template.Must(template.New("letter").Parse(letterTemplates))
