eecc69fbdf85fe6ecd24bdd2bc6f015e19e3461d
neefrankie <neefrankie@gmail.com>
2020-04-19 21:03:17 +0800

    refactor: using iota

--- models/admin/licence_status.go
+++ models/admin/licence_status.go
@@ -2,25 +2,79 @@ package admin
 
 import (
 	"database/sql/driver"
+	"encoding/json"
 	"errors"
+	"fmt"
 )
 
-type LicenceStatus string
+type LicenceStatus int
 
 const (
-	LicStatusNull      LicenceStatus = ""
-	LicStatusAvailable LicenceStatus = "available"
-	LicStatusInvited   LicenceStatus = "invitation_sent"
-	LicStatusGranted   LicenceStatus = "granted"
+	LicStatusNull LicenceStatus = iota
+	LicStatusAvailable
+	LicStatusInvited
+	LicStatusGranted
 )
 
-func (x LicenceStatus) MarshalJSON() ([]byte, error) {
+var _licenceStatusNames = [...]string{
+	"",
+	"available",
+	"invited",
+	"granted",
+}
 
-	if x == "" {
+// String representation of OrderKind
+var _licenceStatusMap = map[LicenceStatus]string{
+	1: _licenceStatusNames[1],
+	2: _licenceStatusNames[2],
+	3: _licenceStatusNames[3],
+}
+
+// Used to get OrderKind from a string.
+var _licenceStatusValue = map[string]LicenceStatus{
+	_licenceStatusNames[1]: 1,
+	_licenceStatusNames[2]: 2,
+	_licenceStatusNames[3]: 3,
+}
+
+// ParseLicenceStatus creates OrderKind from a string.
+func ParseLicenceStatus(name string) (LicenceStatus, error) {
+	if x, ok := _licenceStatusValue[name]; ok {
+		return x, nil
+	}
+
+	return LicStatusNull, fmt.Errorf("%s is not valid LicenceStatus", name)
+}
+
+func (x LicenceStatus) String() string {
+	if s, ok := _licenceStatusMap[x]; ok {
+		return s
+	}
+
+	return ""
+}
+
+func (x *LicenceStatus) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	tmp, _ := ParseLicenceStatus(s)
+
+	*x = tmp
+
+	return nil
+}
+
+func (x LicenceStatus) MarshalJSON() ([]byte, error) {
+	s := x.String()
+
+	if s == "" {
 		return []byte("null"), nil
 	}
 
-	return []byte(`"` + x + `"`), nil
+	return []byte(`"` + s + `"`), nil
 }
 
 func (x *LicenceStatus) Scan(src interface{}) error {
@@ -31,7 +85,8 @@ func (x *LicenceStatus) Scan(src interface{}) error {
 
 	switch s := src.(type) {
 	case []byte:
-		*x = LicenceStatus(s)
+		tmp, _ := ParseLicenceStatus(string(s))
+		*x = tmp
 		return nil
 
 	default:
@@ -40,9 +95,10 @@ func (x *LicenceStatus) Scan(src interface{}) error {
 }
 
 func (x LicenceStatus) Value() (driver.Value, error) {
-	if x == "" {
+	s := x.String()
+	if s == "" {
 		return nil, nil
 	}
 
-	return x, nil
+	return s, nil
 }
