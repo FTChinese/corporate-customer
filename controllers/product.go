cb9a3aa6c59b1dae299de2f4f946dc5e511877a0
weiguo.ni <neefrankie@gmail.com>
2020-04-20 14:31:26 +0800

    refactor: moved product and plan related db operation to separate package

--- controllers/product.go
+++ controllers/product.go
@@ -1,27 +1,27 @@
 package controllers
 
 import (
-	"github.com/FTChinese/b2b/repository"
+	"github.com/FTChinese/b2b/repository/products"
 	"github.com/FTChinese/go-rest/render"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 type ProductRouter struct {
-	repo repository.Env
+	repo products.Env
 }
 
-func NewProductRouter(repo repository.Env) ProductRouter {
+func NewProductRouter(repo products.Env) ProductRouter {
 	return ProductRouter{repo: repo}
 }
 
 // ListProducts loads all products.
 func (router ProductRouter) ListProducts(c echo.Context) error {
-	products, err := router.repo.LoadProducts()
+	prod, err := router.repo.LoadProducts()
 
 	if err != nil {
 		return render.NewDBError(err)
 	}
 
-	return c.JSON(http.StatusOK, products)
+	return c.JSON(http.StatusOK, prod)
 }
--- repository/products/plans.go
+++ repository/products/plans.go
@@ -1,41 +1,10 @@
-package repository
+package products
 
 import (
 	"github.com/FTChinese/b2b/models/plan"
 	"github.com/FTChinese/b2b/repository/stmt"
 	"strings"
 )
-
-func (env Env) LoadProducts() ([]plan.Product, error) {
-	// TODO: find in cache.
-
-	productRows, err := env.retrieveProducts()
-	if err != nil {
-		return nil, err
-	}
-
-	planIDs := plan.GetProductsPlanIDs(productRows)
-
-	groupedPlans, err := env.PlansInSet(planIDs)
-	if err != nil {
-		return nil, err
-	}
-
-	products := plan.ZipProductWithPlan(productRows, groupedPlans)
-
-	// TODO: cache the final products.
-
-	return products, nil
-}
-
-func (env Env) retrieveProducts() ([]plan.ProductSchema, error) {
-	var rows []plan.ProductSchema
-	if err := env.db.Select(&rows, stmt.Products); err != nil {
-		return nil, err
-	}
-
-	return rows, nil
-}
 
 // PlansInSet loads a list of active plans
 // and returns a map with plan id as key.
