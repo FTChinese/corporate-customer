f06c047afd2e8f94f8ecd5c53f26168cb5073426
neefrankie <neefrankie@gmail.com>
2021-07-11 02:01:07 +0800

    refactor: validator

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -5,38 +5,13 @@ import (
 	"github.com/FTChinese/go-rest/render"
 	"github.com/asaskevich/govalidator"
 	"log"
-	"strings"
-	"unicode/utf8"
 )
 
-// Required checks if a string is empty.
-// Leading and trailing spaces are trimmed.
-func Required(str string) bool {
-	return strings.TrimSpace(str) != ""
-}
-
-// StringInLength checks if a string's length, including multi bytes string,
-// is within a range, inclusive.
-func StringInLength(str string, min, max int) bool {
-	if min > max {
-		min, max = max, min
-	}
-	strLength := utf8.RuneCountInString(str)
-	return strLength >= min && strLength <= max
-}
-
-// MinStringLength checks if a string's length is longer than min
-func MinStringLength(str string, min int) bool {
-	strLength := utf8.RuneCountInString(str)
-	return strLength >= min
-}
-
-// MaxStringLength checks if a string's length is under max
-// Return true if the length of str is under or equal to max; false otherwise
-func MaxStringLength(str string, max int) bool {
-	strLength := utf8.RuneCountInString(str)
-	return strLength <= max
-}
+const (
+	msgTooLong  = "The length of %s should not exceed %d chars"
+	msgTooShort = "The length of %s should not less than %d chars"
+	msgLenRange = "The length of %s must be within %d to %d chars"
+)
 
 type Validator struct {
 	fieldName  string
@@ -58,12 +33,12 @@ func (v *Validator) Required() *Validator {
 	return v
 }
 
-func (v *Validator) Min(min int) *Validator {
+func (v *Validator) MinLen(min int) *Validator {
 	v.min = min
 	return v
 }
 
-func (v *Validator) Max(max int) *Validator {
+func (v *Validator) MaxLen(max int) *Validator {
 	v.max = max
 	return v
 }
@@ -83,12 +58,6 @@ func (v *Validator) URL() *Validator {
 	v.isURL = true
 	return v
 }
-
-const (
-	msgTooLong  = "The length of %s should not exceed %d chars"
-	msgTooShort = "The length of %s should not less than %d chars"
-	msgLenRange = "The length of %s must be within %d to %d chars"
-)
 
 func (v *Validator) Validate(value string) *render.ValidationError {
 	if v.isEmail && v.isURL {
