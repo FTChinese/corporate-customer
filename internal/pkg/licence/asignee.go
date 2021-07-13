839cfba872329b674313fe5695dfb1b2c7a35043
weiguo.ni <neefrankie@gmail.com>
2021-07-13 17:43:34 +0800

    feat: Scan and Value method for Assignee

---
+++ internal/pkg/licence/asignee.go
@@ -0,0 +1,64 @@
+package licence
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"github.com/guregu/null"
+	"strings"
+)
+
+// Assignee represents a reader who can accept
+// an invitation email, and who can be granted
+// a licence.
+type Assignee struct {
+	FtcID    null.String `json:"ftcId" db:"ftc_id"`
+	Email    null.String `json:"email" db:"user_email"`
+	UserName null.String `json:"userName" db:"user_name"`
+}
+
+func (a Assignee) Value() (driver.Value, error) {
+	if a.FtcID.IsZero() {
+		return nil, nil
+	}
+
+	return a.FtcID.String, nil
+}
+
+func (a *Assignee) Scan(src interface{}) error {
+	if src == nil {
+		*a = Assignee{}
+	}
+
+	switch s := src.(type) {
+	case []byte:
+		var tmp Assignee
+		err := json.Unmarshal(s, &tmp)
+		if err != nil {
+			return err
+		}
+		*a = tmp
+		return nil
+
+	default:
+		return errors.New("incompatible type to scan")
+	}
+}
+
+// NormalizeName tries to find a proper way to greet user
+// in email.
+func (a Assignee) NormalizeName() string {
+	if a.UserName.Valid {
+		return a.UserName.String
+	}
+
+	return strings.Split(a.Email.String, "@")[0]
+}
+
+func (a Assignee) TeamMember(teamID string) Staffer {
+	return Staffer{
+		Email:  a.Email.String,
+		TeamID: teamID,
+		FtcID:  a.FtcID,
+	}
+}
