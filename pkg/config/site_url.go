5b4d0a9b32f32d72d99691293fd39c73b3159c3f
weiguo.ni <neefrankie@gmail.com>
2021-07-16 15:43:59 +0800

    feat: define site url

---
+++ pkg/config/site_url.go
@@ -0,0 +1,11 @@
+package config
+
+const (
+	SiteBaseURL           = "https://next.ftacademy.cn"
+	B2BBaseURL            = SiteBaseURL + "/corporate"
+	B2BReaderVerification = B2BBaseURL + "/user/verification"
+)
+
+func B2BVerifyInvitationURL(token string) string {
+	return B2BBaseURL + "/verify-invitation/" + token
+}
