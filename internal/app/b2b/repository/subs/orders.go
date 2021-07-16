1a0a8b037145ff7911fe690df91bc8d3abb9d81c
weiguo.ni <neefrankie@gmail.com>
2021-07-16 16:26:33 +0800

    cleanup

--- internal/app/b2b/repository/subs/orders.go
+++ internal/app/b2b/repository/subs/orders.go
@@ -1,26 +1,13 @@
 package subs
 
 import (
-	"github.com/FTChinese/ftacademy/internal/app/b2b/stmt"
 	"github.com/FTChinese/ftacademy/internal/pkg/order"
-	sq2 "github.com/FTChinese/ftacademy/pkg/sq"
 	gorest "github.com/FTChinese/go-rest"
 )
 
-func (env Env) CreateOrders(o order.OrderList) error {
-	query := stmt.OrderBuilder.Rows(len(o)).Build()
-
-	_, err := env.dbs.Write.Exec(query, sq2.BuildInsertValues(o)...)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func (env Env) LoadOrder(id, teamID string) (order.Order, error) {
 	var o order.Order
-	err := env.dbs.Read.Get(&o, stmt.GetOrder, id, teamID)
+	err := env.dbs.Read.Get(&o, order.GetOrder, id, teamID)
 
 	if err != nil {
 		return o, err
@@ -32,7 +19,7 @@ func (env Env) LoadOrder(id, teamID string) (order.Order, error) {
 func (env Env) ListOrders(teamID string, page gorest.Pagination) ([]order.Order, error) {
 	var o = make([]order.Order, 0)
 
-	err := env.dbs.Read.Select(&o, stmt.ListOrder, teamID, page.Limit, page.Offset())
+	err := env.dbs.Read.Select(&o, order.ListOrder, teamID, page.Limit, page.Offset())
 
 	if err != nil {
 		return o, err
@@ -60,7 +47,7 @@ func (env Env) AsyncListOrders(teamID string, page gorest.Pagination) <-chan ord
 
 func (env Env) CountOrder(teamID string) (int64, error) {
 	var total int64
-	err := env.dbs.Read.Get(&total, stmt.CountOrder, teamID)
+	err := env.dbs.Read.Get(&total, order.CountOrder, teamID)
 	if err != nil {
 		return 0, err
 	}
