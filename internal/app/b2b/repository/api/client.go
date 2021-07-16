9856fe1489e5be527cc2c4b0a79998b35468f48b
weiguo.ni <neefrankie@gmail.com>
2021-07-16 15:54:39 +0800

    feat: api client

---
+++ internal/app/b2b/repository/api/client.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	"github.com/FTChinese/ftacademy/internal/pkg/input"
+	"github.com/FTChinese/ftacademy/pkg/config"
+	"github.com/FTChinese/ftacademy/pkg/fetch"
+	"log"
+	"net/http"
+)
+
+type Client struct {
+	key     string
+	baseURL string
+}
+
+func NewSubsAPIClient(prod bool) Client {
+	return Client{
+		key:     config.MustSubsAPIKey().Pick(prod),
+		baseURL: config.MustSubsAPIv2BaseURL().Pick(prod),
+	}
+}
+
+func (c Client) ReaderSignup(s input.SignupParams) (*http.Response, error) {
+	url := c.baseURL + "/auth/email/signup"
+
+	log.Printf("Forward reader signup to api %s", url)
+
+	resp, errs := fetch.New().
+		Post(url).
+		SetBearerAuth(c.key).
+		SendJSON(s).
+		End()
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+func (c Client) VerifySignup(token string) (*http.Response, error) {
+	url := c.baseURL + "/auth/email/verification/" + token
+
+	log.Printf("Forward reader verification to api %s", url)
+
+	resp, errs := fetch.New().
+		Post(url).
+		SetBearerAuth(c.key).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
