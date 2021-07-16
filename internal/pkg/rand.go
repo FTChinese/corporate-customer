dc34d5e8b5f4999a1aa3985d9522030be8c93be4
weiguo.ni <neefrankie@gmail.com>
2021-07-16 15:47:05 +0800

    feat: id generators

---
+++ internal/pkg/rand.go
@@ -0,0 +1,28 @@
+package pkg
+
+import "github.com/FTChinese/go-rest/rand"
+
+func LicenceID() string {
+	return "lic_" + rand.String(12)
+}
+
+func InvitationID() string {
+	return "invite_" + rand.String(12)
+}
+
+func ReceiptID() string {
+	return "rcpt_" + rand.String(12)
+}
+
+func ReceiptItemID() string {
+	return "rci_" + rand.String(12)
+}
+
+func SnapshotID() string {
+	return "snp_" + rand.String(12)
+}
+
+// InvoiceID creates an id for addon invoice.
+func InvoiceID() string {
+	return "inv_" + rand.String(12)
+}
