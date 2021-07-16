3c9177a121aa17f87c9af5f017cbf5f9157cad01
weiguo.ni <neefrankie@gmail.com>
2021-07-16 15:55:14 +0800

    feat: handle invitation

--- internal/app/b2b/controller/subs_invitation.go
+++ internal/app/b2b/controller/subs_invitation.go
@@ -2,9 +2,11 @@ package controller
 
 import (
 	"github.com/FTChinese/ftacademy/internal/app/b2b/repository/subs"
+	"github.com/FTChinese/ftacademy/internal/pkg/admin"
 	"github.com/FTChinese/ftacademy/internal/pkg/input"
 	"github.com/FTChinese/ftacademy/internal/pkg/letter"
 	"github.com/FTChinese/ftacademy/internal/pkg/licence"
+	"github.com/FTChinese/ftacademy/internal/pkg/reader"
 	gorest "github.com/FTChinese/go-rest"
 	"github.com/FTChinese/go-rest/render"
 	"github.com/labstack/echo/v4"
@@ -12,11 +14,16 @@ import (
 )
 
 // CreateInvitation creates an invitation for a licence and send it to a user.
-// Input: {email: string, description: string, licenceId: string}
+// Input:
+// email: string,
+// description: string,
+// licenceId: string
+// teamId: string
 func (router SubsRouter) CreateInvitation(c echo.Context) error {
 	defer router.logger.Sync()
 	sugar := router.logger.Sugar()
 
+	// TODO: ensure TeamID exists.
 	claims := getPassportClaims(c)
 
 	var params input.InvitationParams
@@ -27,14 +34,9 @@ func (router SubsRouter) CreateInvitation(c echo.Context) error {
 	if ve := params.Validate(); ve != nil {
 		return render.NewUnprocessable(ve)
 	}
+	params.TeamID = claims.TeamID.String
 
-	inv, err := licence.NewInvitation(params, claims)
-	if err != nil {
-		sugar.Error(err)
-		return render.NewInternalError(err.Error())
-	}
-
-	invitedLicence, err := router.repo.CreateInvitation(inv)
+	lic, err := router.repo.CreateInvitation(params, claims.AdminID)
 	if err != nil {
 		switch err {
 		case subs.ErrLicenceUnavailable:
@@ -65,16 +67,23 @@ func (router SubsRouter) CreateInvitation(c echo.Context) error {
 
 	// Send invitation letter
 	go func() {
-
+		assignee, err := router.repo.FindAssignee(params.Email)
+		if err != nil {
+			sugar.Error(err)
+			return
+		}
 		adminProfile, err := router.repo.AdminProfile(claims.AdminID)
 		if err != nil {
 			sugar.Error(err)
 			return
 		}
 
-		parcel, err := letter.InvitationParcel(invitedLicence, adminProfile)
+		parcel, err := letter.InvitationParcel(
+			assignee,
+			lic,
+			adminProfile)
 		if err != nil {
-			sugar.Error()
+			sugar.Error(err)
 			return
 		}
 
@@ -84,27 +93,27 @@ func (router SubsRouter) CreateInvitation(c echo.Context) error {
 		}
 	}()
 
-	// Add the invitee to team member
-	go func() {
-		_ = router.repo.SaveStaffer(invitedLicence.Assignee.TeamMember(claims.TeamID.String))
-	}()
-
-	return c.NoContent(http.StatusNoContent)
+	return c.JSON(http.StatusOK, licence.Licence{
+		BaseLicence: lic,
+		Assignee:    licence.Assignee{},
+	})
 }
 
-// RevokeInvitation cancels an invitation before it is accepted by user.
+// RevokeInvitation revokes the invitation of a licence
+// before the licence is granted.
 // If the invitation is already accepted, revoke has no effect.
 // Admin should revoke a licence for this purpose.
 func (router SubsRouter) RevokeInvitation(c echo.Context) error {
 	invID := c.Param("id") // the invitation id
 	claims := getPassportClaims(c)
 
-	err := router.repo.RevokeInvitation(invID, claims.TeamID.String)
+	inv, err := router.repo.RevokeInvitation(invID, claims.TeamID.String)
+	// TODO: handle different errors
 	if err != nil {
 		return render.NewDBError(err)
 	}
 
-	return c.NoContent(http.StatusNoContent)
+	return c.JSON(http.StatusOK, inv)
 }
 
 // ListInvitations shows all invitations
@@ -117,13 +126,71 @@ func (router SubsRouter) ListInvitations(c echo.Context) error {
 		return render.NewBadRequest(err.Error())
 	}
 
-	countCh, listCh := router.repo.AsyncCountInvitation(claims.TeamID.String), router.repo.AsyncListInvitations(claims.TeamID.String, page)
-
-	countResult, listResult := <-countCh, <-listCh
-	if listResult.Err != nil {
-		return render.NewDBError(listResult.Err)
+	invs, err := router.repo.ListInvitations(claims.TeamID.String, page)
+	if err != nil {
+		return render.NewDBError(err)
 	}
 
-	listResult.Total = countResult.Total
-	return c.JSON(http.StatusOK, listResult)
+	return c.JSON(http.StatusOK, invs)
+}
+
+func (router SubsRouter) VerifyInvitation(c echo.Context) error {
+	defer router.logger.Sync()
+	sugar := router.logger.Sugar()
+
+	token := c.QueryParam("token")
+
+	inv, err := router.repo.InvitationByToken(token)
+	if err != nil {
+		sugar.Error(err)
+		// 404 Not Found
+		return render.NewDBError(err)
+	}
+
+	// Ensure the invitation is usable.
+	if !inv.IsAcceptable() {
+		return render.NewBadRequest("invitation is either used or expired")
+	}
+
+	lic, err := router.repo.LoadLicence(admin.AccessRight{
+		RowID:  inv.LicenceID,
+		TeamID: inv.TeamID,
+	})
+	if err != nil {
+		sugar.Error(err)
+		// 404 Not Found
+		return render.NewDBError(err)
+	}
+	// TODO: remove this or keep it? We could leave it to the client to determine whether the licence is available.
+	if !lic.IsAvailable() {
+		sugar.Error(err)
+		return render.NewBadRequest("Licence is not available to be granted")
+	}
+
+	// Find user by invitation email.
+	// If user is not found, it only indicates user is not
+	// signed-up.
+	assignee, err := router.repo.FindAssignee(inv.Email)
+	// 404 won't happen here
+	if err != nil {
+		return render.NewDBError(err)
+	}
+	if assignee.IsZero() {
+		c.JSON(http.StatusOK, licence.InvitationVerified{
+			Licence:    lic,
+			Assignee:   assignee,
+			Membership: reader.Membership{},
+		})
+	}
+
+	m, err := router.repo.RetrieveMembership(assignee.FtcID.String)
+	if err != nil {
+		return render.NewDBError(err)
+	}
+
+	return c.JSON(http.StatusOK, licence.InvitationVerified{
+		Licence:    lic,
+		Assignee:   assignee,
+		Membership: m,
+	})
 }
