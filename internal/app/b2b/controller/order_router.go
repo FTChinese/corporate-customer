faa66ba56dbb4243332410aa7449f2a3477d634c
weiguo.ni <neefrankie@gmail.com>
2021-07-16 16:27:13 +0800

    rewrite

--- internal/app/b2b/controller/order_router.go
+++ internal/app/b2b/controller/order_router.go
@@ -1,69 +1,33 @@
 package controller
 
 import (
-	"github.com/FTChinese/ftacademy/internal/app/b2b/repository/products"
 	"github.com/FTChinese/ftacademy/internal/app/b2b/repository/subs"
-	"github.com/FTChinese/ftacademy/internal/pkg/admin"
-	"github.com/FTChinese/ftacademy/internal/pkg/order"
-	"github.com/FTChinese/ftacademy/pkg/postman"
+	"github.com/FTChinese/ftacademy/pkg/db"
 	"github.com/FTChinese/go-rest"
 	"github.com/FTChinese/go-rest/render"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 type OrderRouter struct {
-	repo         subs.Env
-	productsRepo products.Env
-	post         postman.Postman
+	repo   subs.Env
+	logger *zap.Logger
 }
 
-func NewOrderRouter(env subs.Env, prodRepo products.Env, p postman.Postman) OrderRouter {
+func NewOrderRouter(dbs db.ReadWriteMyDBs, logger *zap.Logger) OrderRouter {
 	return OrderRouter{
-		repo:         env,
-		productsRepo: prodRepo,
-		post:         p,
+		repo: subs.NewEnv(dbs, logger),
 	}
 }
 
 // CreateOrders creates orders an org purchased.
 // Client should specify which plans are being subscribed,
 // and how many copies.
-// TODO: design the data structure of a cart.
-// Input:
-// [
-//  {planId: string, quantity: number, cycleCount: number, kind: "create" },
-//  {planId: string, cycleCount: number, kind: "renew", licences: string[] }
-//	{planId: string, cycleCount: number, kind: "upgrade", licences: string[] }
-// ]
-// At most there should be two plans: a standard and a premium.
 func (router OrderRouter) CreateOrders(c echo.Context) error {
 	claims := getPassportClaims(c)
 
-	var cartItems []order.CartItem
-	if err := c.Bind(&cartItems); err != nil {
-		return render.NewBadRequest(err.Error())
-	}
-
-	plans, err := router.productsRepo.PlansInSet(order.GetCartPlanIDs(cartItems))
-
-	if err != nil {
-		return render.NewDBError(err)
-	}
-
-	cart, ve := order.NewCart(cartItems, plans)
-	if ve != nil {
-		return render.NewUnprocessable(ve)
-	}
-
-	orders := cart.BuildOrders(claims.TeamID.String)
-
-	err = router.repo.CreateOrders(orders)
-	if err != nil {
-		return render.NewDBError(err)
-	}
-
-	return c.NoContent(http.StatusNoContent)
+	return c.JSON(http.StatusOK, claims)
 }
 
 func (router OrderRouter) ListOrders(c echo.Context) error {
@@ -74,13 +38,5 @@ func (router OrderRouter) ListOrders(c echo.Context) error {
 		return render.NewBadRequest(err.Error())
 	}
 
-	listCh, countCh := router.repo.AsyncListOrders(claims.TeamID.String, page), router.repo.AsyncCountOrder(admin.StmtTeamByID)
-
-	listResult, countResult := <-listCh, <-countCh
-	if listResult.Err != nil {
-		return render.NewDBError(listResult.Err)
-	}
-
-	listResult.Total = countResult.Total
-	return c.JSON(http.StatusOK, listResult)
+	return c.JSON(http.StatusOK, claims)
 }
