dcecd025b5cc027d1ff5661cbe46bdb92f2c0e05
neefrankie <neefrankie@gmail.com>
2021-07-19 00:06:16 +0800

    feat: redefine order

--- internal/pkg/order/order.go
+++ internal/pkg/order/order.go
@@ -1,22 +1,21 @@
 package order
 
 import (
-	plan2 "github.com/FTChinese/ftacademy/pkg/plan"
+	"github.com/FTChinese/ftacademy/internal/pkg"
+	"github.com/FTChinese/ftacademy/internal/pkg/admin"
 	sq2 "github.com/FTChinese/ftacademy/pkg/sq"
 	"github.com/FTChinese/go-rest/chrono"
-	"github.com/FTChinese/go-rest/enum"
-	"github.com/FTChinese/go-rest/rand"
 	"github.com/guregu/null"
 )
 
 // CartItem is the plan user is subscribing
 // and the number of copies for this plan.
-type CartItem struct {
-	PlanID     string             `json:"planId"`
-	Quantity   int64              `json:"quantity"`
-	CycleCount int64              `json:"cycleCount"`
-	Plan       plan2.DiscountPlan `json:"-"`
-}
+//type CartItem struct {
+//	PlanID     string             `json:"planId"`
+//	Quantity   int64              `json:"quantity"`
+//	CycleCount int64              `json:"cycleCount"`
+//	Plan       plan2.DiscountPlan `json:"-"`
+//}
 
 type CheckoutItem struct {
 	CartItem
@@ -34,18 +33,18 @@ type Cart struct {
 	Items      []CartItem
 }
 
-func (c Cart) BuildOrders(teamID string) OrderList {
-	var orders []Order
-
-	for _, v := range c.Items {
-		for i := 0; i < int(v.Quantity); i++ {
-			o := NewOrder(v, teamID, c.CheckoutID)
-			orders = append(orders, o)
-		}
-	}
-
-	return orders
-}
+//func (c Cart) BuildOrders(teamID string) OrderList {
+//	var orders []Order
+//
+//	for _, v := range c.Items {
+//		for i := 0; i < int(v.Quantity); i++ {
+//			o := NewOrder(v, teamID, c.CheckoutID)
+//			orders = append(orders, o)
+//		}
+//	}
+//
+//	return orders
+//}
 
 // Order describes the details of each transaction
 // to purchase a licence.
@@ -60,54 +59,75 @@ func (c Cart) BuildOrders(teamID string) OrderList {
 // upgraded and the membership (if the licence is granted
 // to someone) will be backed up and updated corresponding.
 type Order struct {
-	ID           string         `db:"order_id"`
-	PlanID       string         `db:"plan_id"`
-	DiscountID   null.Int       `db:"discount_id"`
-	LicenceID    string         `db:"licence_id"`
-	TeamID       string         `db:"team_id"`
-	CheckoutID   string         `db:"checkout_id"`
-	Amount       float64        `db:"amount"`
-	CycleCount   int64          `db:"cycle_count"`
-	TrialDays    int64          `db:"trial_days"`
-	Kind         enum.OrderKind `db:"kind"`
-	PeriodStart  chrono.Date    `db:"period_start"`
-	PeriodEnd    chrono.Date    `db:"period_end"`
-	CreatedUTC   chrono.Time    `db:"created_utc"`
-	ConfirmedUTC chrono.Time    `db:"confirmed_utc"`
+	ID             string          `db:"order_id"`
+	AmountPayable  float64         `json:"amountPayable"`
+	CreatedBy      string          `json:"createdBy"`
+	CreatedUTC     chrono.Time     `json:"createdUtc"`
+	Status         Status          `json:"status"`
+	ItemCount      int64           `json:"itemCount"`
+	CartItemsBrief []CartItemBrief `json:"cart_items_brief"` // An array of products, together with the quantities, use is trying to purchase.
+	TeamID         string          `db:"team_id"`
+	// The following fields exists only after payment confirmed.
+	AmountPaid    null.Float    `json:"amountPaid"`
+	ApprovedBy    null.String   `json:"approvedBy"`
+	ApprovedUTC   chrono.Time   `json:"approvedUtc"`
+	SellerNote    null.String   `json:"sellerNote"`
+	PaymentMethod PaymentMethod `json:"paymentMethod"`
+	TransactionID null.String   `json:"transactionId"`
 }
 
-func NewOrder(item CartItem, teamID, checkoutID string) Order {
+func NewOrder(cart ShoppingCart, p admin.PassportClaims) Order {
+
 	return Order{
-		ID:           "ord_" + rand.String(12),
-		PlanID:       item.PlanID,
-		DiscountID:   item.Plan.DiscountID,
-		LicenceID:    "lic_" + rand.String(12),
-		TeamID:       teamID,
-		CheckoutID:   checkoutID,
-		Amount:       item.Plan.PayableAmount(),
-		CycleCount:   item.CycleCount,
-		TrialDays:    7,
-		Kind:         enum.OrderKindCreate,
-		PeriodStart:  chrono.Date{},
-		PeriodEnd:    chrono.Date{},
-		CreatedUTC:   chrono.TimeNow(),
-		ConfirmedUTC: chrono.Time{},
+		ID:             pkg.OrderID(),
+		AmountPayable:  cart.TotalAmount,
+		CreatedBy:      p.AdminID,
+		CreatedUTC:     chrono.TimeNow(),
+		Status:         StatusPending,
+		ItemCount:      cart.ItemCount,
+		CartItemsBrief: cart.ItemsBrief(),
+		TeamID:         p.TeamID.String,
+		AmountPaid:     null.Float{},
+		ApprovedBy:     null.String{},
+		ApprovedUTC:    chrono.Time{},
+		SellerNote:     null.String{},
+		PaymentMethod:  "",
+		TransactionID:  null.String{},
 	}
 }
 
+//func NewOrder(item CartItem, teamID, checkoutID string) Order {
+//	return Order{
+//		ID:           "ord_" + rand.String(12),
+//		PlanID:       item.PlanID,
+//		DiscountID:   item.Plan.DiscountID,
+//		LicenceID:    "lic_" + rand.String(12),
+//		TeamID:       teamID,
+//		CheckoutID:   checkoutID,
+//		Amount:       item.Plan.PayableAmount(),
+//		CycleCount:   item.CycleCount,
+//		TrialDays:    7,
+//		Kind:         enum.OrderKindCreate,
+//		PeriodStart:  chrono.Date{},
+//		PeriodEnd:    chrono.Date{},
+//		CreatedUTC:   chrono.TimeNow(),
+//		ConfirmedUTC: chrono.Time{},
+//	}
+//}
+
 func (o Order) RowValues() []interface{} {
 	return []interface{}{
-		o.ID,
-		o.PlanID,
-		o.DiscountID,
-		o.LicenceID,
-		o.TeamID,
-		o.CheckoutID,
-		o.Amount,
-		o.CycleCount,
-		o.TrialDays,
-		o.Kind,
-		"UTC_TIMESTAMP()",
+		//o.ID,
+		//o.PlanID,
+		//o.DiscountID,
+		//o.LicenceID,
+		//o.TeamID,
+		//o.CheckoutID,
+		//o.Amount,
+		//o.CycleCount,
+		//o.TrialDays,
+		//o.Kind,
+		//"UTC_TIMESTAMP()",
 	}
 }
 
@@ -119,7 +139,7 @@ func (ol OrderList) Each(handler func(row sq2.InsertRow)) {
 	}
 }
 
-// PageOrders contains the count of total orders of a team,
+// PagedOrders contains the count of total orders of a team,
 // and the current page of orders.
 type PagedOrders struct {
 	Total int64   `json:"total"`
--- internal/pkg/order/shopping_cart.go
+++ internal/pkg/order/shopping_cart.go
@@ -1,18 +1,63 @@
 package order
 
 import (
+	"github.com/FTChinese/ftacademy/internal/pkg"
 	"github.com/FTChinese/ftacademy/internal/pkg/licence"
 	"github.com/FTChinese/ftacademy/pkg/price"
+	"github.com/guregu/null"
 )
 
-type CarItem struct {
+// CartItemBrief gives a brief statistics of CartItem.
+// This is used when save all items of an order together in the's row.
+type CartItemBrief struct {
+	Price         price.Price `json:"price"`
+	NewCopies     int64       `json:"newCopies"`     // How many new copies user purchased
+	RenewalCopies int64       `json:"renewalCopies"` // How many renewals user purchased.
+}
+
+// CartItemSchema describes how a CartItem is saved in db.
+// Each item in the shopping cart's items array is treated as a SQL row.
+type CartItemSchema struct {
+	ID      string
+	OrderID string
+	CartItem
+	PriceOffPerCopy null.Float
+}
+
+type CartItem struct {
 	Price     price.Price       `json:"price"`
 	NewCopies int64             `json:"newCopies"`
 	Renewals  []licence.Licence `json:"renewals"`
 }
 
+func (i CartItem) Brief() CartItemBrief {
+	return CartItemBrief{
+		Price:         i.Price,
+		NewCopies:     i.NewCopies,
+		RenewalCopies: int64(len(i.Renewals)),
+	}
+}
+
+func (i CartItem) Schema(orderID string) CartItemSchema {
+	return CartItemSchema{
+		ID:              pkg.OrderItemID(),
+		OrderID:         orderID,
+		CartItem:        i,
+		PriceOffPerCopy: null.Float{},
+	}
+}
+
 type ShoppingCart struct {
-	Items       []CarItem `json:"items"`
-	ItemCount   int64     `json:"itemCount"`
-	TotalAmount int64     `json:"total_amount"`
+	Items       []CartItem `json:"items"`
+	ItemCount   int64      `json:"itemCount"`
+	TotalAmount float64    `json:"totalAmount"`
+}
+
+func (s *ShoppingCart) ItemsBrief() []CartItemBrief {
+	var items = make([]CartItemBrief, 0)
+	for _, v := range s.Items {
+		items = append(items, v.Brief())
+	}
+
+	return items
 }
