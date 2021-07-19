7f409bdc8ce75e7858a5bf3f9e5feb83279cb33d
weiguo.ni <neefrankie@gmail.com>
2021-07-19 14:12:14 +0800

    feat: implement Valuer interface

--- internal/pkg/order/payment_method.go
+++ internal/pkg/order/payment_method.go
@@ -1,10 +1,40 @@
 package order
 
+import (
+	"database/sql/driver"
+	"errors"
+)
+
 type PaymentMethod string
 
 const (
+	PaymentMethodNULL   PaymentMethod = ""
 	PaymentMethodBank   PaymentMethod = "bank"
 	PaymentMethodStripe PaymentMethod = "stripe"
 	PaymentMethodAli    PaymentMethod = "alipay"
 	PaymentMethodWx     PaymentMethod = "wechat"
 )
+
+func (p PaymentMethod) Value() (driver.Value, error) {
+	if p == "" {
+		return nil, nil
+	}
+
+	return string(p), nil
+}
+
+func (p *PaymentMethod) Scan(src interface{}) error {
+	if src == nil {
+		*p = PaymentMethodNULL
+		return nil
+	}
+
+	switch s := src.(type) {
+	case []byte:
+		*p = PaymentMethod(s)
+		return nil
+
+	default:
+		return errors.New("incompatible type to scan to PaymentMethod")
+	}
+}
