4df4a9653a0fabcb3aacbb3b1ef3b28fe8179e0f
weiguo.ni <neefrankie@gmail.com>
2021-07-19 14:12:27 +0800

    feat: shopping cart

--- internal/pkg/order/shopping_cart.go
+++ internal/pkg/order/shopping_cart.go
@@ -1,37 +1,47 @@
 package order
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
 	"github.com/FTChinese/ftacademy/internal/pkg"
 	"github.com/FTChinese/ftacademy/internal/pkg/licence"
 	"github.com/FTChinese/ftacademy/pkg/price"
+	"github.com/FTChinese/ftacademy/pkg/sq"
 	"github.com/guregu/null"
 )
 
-// CartItemBrief gives a brief statistics of CartItem.
-// This is used when save all items of an order together in the's row.
-type CartItemBrief struct {
-	Price         price.Price `json:"price"`
-	NewCopies     int64       `json:"newCopies"`     // How many new copies user purchased
-	RenewalCopies int64       `json:"renewalCopies"` // How many renewals user purchased.
+// ShoppingCart is used to hold data submitted by client.
+// When storing a shopping cart, its data is decomposed:
+// A summary of items is save in an order row;
+// the items array is save into a separate table one by one.
+// Table `order` to `order_item` has a one-many relation.
+type ShoppingCart struct {
+	Items       []CartItem `json:"items"`
+	ItemCount   int64      `json:"itemCount"`
+	TotalAmount float64    `json:"totalAmount"`
 }
 
-// CartItemSchema describes how a CartItem is saved in db.
-// Each item in the shopping cart's items array is treated as a SQL row.
-type CartItemSchema struct {
-	ID      string
-	OrderID string
-	CartItem
-	PriceOffPerCopy null.Float
+func (s *ShoppingCart) ProductsBrief() []CheckoutProduct {
+	var items = make([]CheckoutProduct, 0)
+	for _, v := range s.Items {
+		items = append(items, v.ProductBrief())
+	}
+
+	return items
 }
 
+// CartItem describes how many copies user purchased a product.
+// Saved in b2b.order_item table with Price and Renewals fields as JSON.
 type CartItem struct {
-	Price     price.Price       `json:"price"`
-	NewCopies int64             `json:"newCopies"`
-	Renewals  []licence.Licence `json:"renewals"`
+	Price     price.Price     `json:"price" db:"price_snapshot"`
+	NewCopies int64           `json:"newCopies" db:"new_copy_count"`
+	Renewals  RenewalLicences `json:"renewals" db:"renewal_licences"`
 }
 
-func (i CartItem) Brief() CartItemBrief {
-	return CartItemBrief{
+// ProductBrief gives an overview of a product put in cart.
+func (i CartItem) ProductBrief() CheckoutProduct {
+	return CheckoutProduct{
 		Price:         i.Price,
 		NewCopies:     i.NewCopies,
 		RenewalCopies: int64(len(i.Renewals)),
@@ -47,17 +57,63 @@ func (i CartItem) Schema(orderID string) CartItemSchema {
 	}
 }
 
-type ShoppingCart struct {
-	Items       []CartItem `json:"items"`
-	ItemCount   int64      `json:"itemCount"`
-	TotalAmount float64    `json:"totalAmount"`
+// CartItemSchema describes how a CartItem is saved in db.
+// Each item in the shopping cart's items array is treated as a SQL row.
+type CartItemSchema struct {
+	ID              string     `json:"id" db:"order_item_id"`
+	OrderID         string     `json:"orderId" db:"order_id"`
+	PriceOffPerCopy null.Float `json:"priceOffPerCopy" db:"price_off_per_copy"`
+	CartItem
 }
 
-func (s *ShoppingCart) ItemsBrief() []CartItemBrief {
-	var items = make([]CartItemBrief, 0)
-	for _, v := range s.Items {
-		items = append(items, v.Brief())
+func (s CartItemSchema) RowValues() []interface{} {
+	return []interface{}{
+		s.ID,
+		s.OrderID,
+		s.CartItem,
+		s.PriceOffPerCopy,
+	}
+}
+
+// RenewalLicences to ease retrieve/save an array of
+// licences into db.
+type RenewalLicences []licence.Licence
+
+func (rl RenewalLicences) Value() (driver.Value, error) {
+	j, err := json.Marshal(rl)
+	if err != nil {
+		return nil, err
 	}
 
-	return items
+	return string(j), nil
+}
+
+func (rl *RenewalLicences) Scan(src interface{}) error {
+	if src == nil {
+		*rl = []licence.Licence{}
+		return nil
+	}
+	switch s := src.(type) {
+	case []byte:
+		var tmp []licence.Licence
+		err := json.Unmarshal(s, &tmp)
+		if err != nil {
+			return err
+		}
+		*rl = tmp
+		return nil
+
+	default:
+		return errors.New("incompatible type to scan to []Licence")
+	}
+}
+
+type CartItems []CartItemSchema
+
+// Each implements Enumerable interface.
+// Usage: `BuildInsertValues(CartItems)...`
+func (ci CartItems) Each(handler func(row sq.InsertRow)) {
+	for _, c := range ci {
+		handler(c)
+	}
 }
