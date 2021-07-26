14f04816c851e16ff622d2c788c575722c8b86ac
weiguo.ni <neefrankie@gmail.com>
2021-07-26 17:41:23 +0800

    feat: NewRowMeta

--- internal/pkg/licence/creator.go
+++ internal/pkg/licence/creator.go
@@ -1,6 +1,9 @@
 package licence
 
-import "github.com/FTChinese/go-rest/chrono"
+import (
+	"github.com/FTChinese/go-rest/chrono"
+	"time"
+)
 
 type Creator struct {
 	CreatorID string `json:"creatorId" db:"creator_id"`
@@ -10,4 +13,13 @@ type Creator struct {
 type RowMeta struct {
 	CreatedUTC chrono.Time `json:"createdUtc" db:"created_utc"`
 	UpdatedUTC chrono.Time `json:"updatedUtc" db:"updated_utc"`
+}
+
+func NewRowMeta() RowMeta {
+	now := time.Now().Truncate(time.Second).In(time.UTC)
+
+	return RowMeta{
+		CreatedUTC: chrono.TimeUTCFrom(now),
+		UpdatedUTC: chrono.TimeUTCFrom(now),
+	}
 }
