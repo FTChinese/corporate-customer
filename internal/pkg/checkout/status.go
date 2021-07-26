cd0e9a2b09e03fde42508820cd1d43d219b7014f
weiguo.ni <neefrankie@gmail.com>
2021-07-26 10:12:48 +0800

    fix: value StatusPending changed to pending_payment

--- internal/pkg/checkout/status.go
+++ internal/pkg/checkout/status.go
@@ -3,7 +3,7 @@ package checkout
 type Status string
 
 const (
-	StatusPending    Status = "pending"
+	StatusPending    Status = "pending_payment"
 	StatusPaid       Status = "paid"
 	StatusProcessing Status = "processing"
 	StatusCancelled  Status = "cancelled"
