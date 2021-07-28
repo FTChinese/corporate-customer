94c06ed76972b7a302513322d2aee176d5e7fdba
weiguo.ni <neefrankie@gmail.com>
2021-07-28 09:55:55 +0800

    mocking data

---
+++ internal/pkg/reader/mock.go
@@ -0,0 +1,40 @@
+// +build !production
+
+package reader
+
+import (
+	"github.com/FTChinese/ftacademy/pkg/addon"
+	"github.com/FTChinese/ftacademy/pkg/price"
+	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/enum"
+	"github.com/google/uuid"
+	"github.com/guregu/null"
+	"time"
+)
+
+func MockMembership() Membership {
+	id := uuid.New().String()
+	return Membership{
+		UserIDs: UserIDs{
+			CompoundID: id,
+			FtcID:      null.StringFrom(id),
+		},
+		Edition: price.Edition{
+			Tier:  enum.TierStandard,
+			Cycle: enum.CycleYear,
+		},
+		LegacyTier:    null.Int{},
+		LegacyExpire:  null.Int{},
+		ExpireDate:    chrono.DateUTCFrom(time.Now().AddDate(0, 1, 0)),
+		PaymentMethod: enum.PayMethodAli,
+		FtcPlanID:     null.String{},
+		StripeSubsID:  null.String{},
+		StripePlanID:  null.String{},
+		AutoRenewal:   false,
+		Status:        0,
+		AppleSubsID:   null.String{},
+		B2BLicenceID:  null.String{},
+		AddOn:         addon.AddOn{},
+		VIP:           false,
+	}.Sync()
+}
