62fd66e69def669a789f4fae8e5d74b04904d483
weiguo.ni <neefrankie@gmail.com>
2021-08-06 17:17:35 +0800

    fix: read config file to byte

--- pkg/db/mock.go
+++ pkg/db/mock.go
@@ -2,9 +2,28 @@
 
 package db
 
-import "github.com/FTChinese/ftacademy/pkg/config"
+import (
+	"github.com/FTChinese/ftacademy/pkg/config"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func readConfigFile() ([]byte, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadFile(filepath.Join(home, "config", "api.toml"))
+}
 
 func MockMySQL() ReadWriteMyDBs {
-	config.MustSetupViper()
+	b, err := readConfigFile()
+	if err != nil {
+		panic(err)
+	}
+
+	config.MustSetupViper(b)
 	return MustNewMyDBs(false)
 }
