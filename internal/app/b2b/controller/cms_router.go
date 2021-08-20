8128a8e5c5ad6b74d696b447077d3d7c10796f24
weiguo.ni <neefrankie@gmail.com>
2021-08-20 10:27:07 +0800

    feat: CMSRouter

--- internal/app/b2b/controller/cms_router.go
+++ internal/app/b2b/controller/cms_router.go
@@ -1,22 +1,22 @@
 package controller
 
 import (
-	"github.com/FTChinese/ftacademy/internal/app/b2b/repository/adminrepo"
+	"github.com/FTChinese/ftacademy/internal/app/b2b/repository/cmsrepo"
 	"github.com/FTChinese/ftacademy/pkg/db"
 	"github.com/FTChinese/ftacademy/pkg/postman"
 	"go.uber.org/zap"
 )
 
 type CMSRouter struct {
-	adminRepo adminrepo.Env
-	post      postman.Postman
-	logger    *zap.Logger
+	repo   cmsrepo.Env
+	post   postman.Postman
+	logger *zap.Logger
 }
 
 func NewCMSRouter(dbs db.ReadWriteMyDBs, p postman.Postman, logger *zap.Logger) CMSRouter {
 	return CMSRouter{
-		adminRepo: adminrepo.NewEnv(dbs, logger),
-		post:      p,
-		logger:    logger,
+		repo:   cmsrepo.NewEnv(dbs, logger),
+		post:   p,
+		logger: logger,
 	}
 }
