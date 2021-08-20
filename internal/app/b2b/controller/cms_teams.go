5bb6061de04b72fd18811a8f8347e388ff0e664d
weiguo.ni <neefrankie@gmail.com>
2021-08-20 10:26:55 +0800

    feat: LoadTeam

--- internal/app/b2b/controller/cms_teams.go
+++ internal/app/b2b/controller/cms_teams.go
@@ -1,1 +1,24 @@
 package controller
+
+import (
+	"github.com/FTChinese/go-rest/render"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+// LoadTeam retrieves a b2b team by the id specified
+// in url path parameters.
+func (router CMSRouter) LoadTeam(c echo.Context) error {
+	defer router.logger.Sync()
+	sugar := router.logger.Sugar()
+
+	teamID := c.Param("id")
+
+	t, err := router.repo.LoadTeam(teamID)
+	if err != nil {
+		sugar.Error(err)
+		return render.NewDBError(err)
+	}
+
+	return c.JSON(http.StatusOK, t)
+}
