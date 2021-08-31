d1462e118e4828201390880ac1f5ff0b40471202
weiguo.ni <neefrankie@gmail.com>
2021-08-31 12:45:03 +0800

    refactor: rename creator_id to admin_id

--- internal/pkg/licence/licence_stmt.go
+++ internal/pkg/licence/licence_stmt.go
@@ -4,7 +4,7 @@ const colLicence = `
 SELECT l.id AS licence_id,
 	l.tier AS tier,
 	l.cycle AS cycle,
-	l.creator_id AS creator_id,
+	l.admin_id AS admin_id,
 	l.team_id AS team_id,
 	l.current_status AS lic_status,
 	l.current_period_start_utc AS current_period_start_utc,
@@ -42,12 +42,23 @@ SELECT COUNT(*) AS total_licence
 FROM b2b.licence
 WHERE team_id = ?`
 
-// StmtLockLicence retrieve a row from licence table and lock it for update.
-const StmtLockLicence = colLicence + `
+// StmtLockBaseLicence locks a row from licence table when
+// admin updates it.
+const StmtLockBaseLicence = colLicence + `
 FROM b2b.licence AS l
 WHERE l.id = ? AND l.team_id = ?
 LIMIT 1
-FOR UPDATE`
+FOR UPDATE
+`
+
+// StmtLockBaseLicenceCMS locks a row in licence when
+// updating it from CMS
+const StmtLockBaseLicenceCMS = colLicence + `
+FROM b2b.licence AS l
+WHERE l.id = ?
+LIMIT 1
+FOR UPDATE
+`
 
 const StmtUpdateLicenceStatus = `
 UPDATE b2b.licence
@@ -57,4 +68,36 @@ SET current_status = :lic_status,
 	updated_utc = :updated_utc
 WHERE id = :licence_id 
 	AND team_id = :team_id
+LIMIT 1`
+
+const colLicenceUpsert = `
+current_period_start_utc = :current_period_start_utc,
+current_period_end_utc = :current_period_end_utc,
+latest_order_id = :latest_order_id,
+latest_price = :latest_price,
+updated_utc = :updated_utc`
+
+const StmtCreateLicence = `
+INSERT INTO b2b.licence
+SET id = :licence_id,
+	tier = :tier,
+	cycle = :cycle,
+	current_status = :lic_status,
+	creator_id = :creator_id,
+	team_id = :team_id,
+
+	start_date_utc = :start_date_utc,
+	trial_start_utc = :trial_start_utc,
+	trial_end_utc = :trial_end_utc,
+	
+	latest_invitation = :latest_invitation,
+	assignee_id = :assignee_id,
+	created_utc = :created_utc,
+` + colLicenceUpsert
+
+const StmtRenewLicence = `
+UPDATE b2b.licence
+SET 
+` + colLicenceUpsert + `
+WHERE licence_id = :licence_id
 LIMIT 1`
