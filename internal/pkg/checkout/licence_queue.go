00471f37cc1c70004f633f0556109ae4645b9d4a
weiguo.ni <neefrankie@gmail.com>
2021-08-31 13:13:59 +0800

    feat: licence queue table

---
+++ internal/pkg/checkout/licence_queue.go
@@ -0,0 +1,110 @@
+package checkout
+
+import (
+	"github.com/FTChinese/ftacademy/internal/pkg"
+	"github.com/FTChinese/ftacademy/internal/pkg/licence"
+	"github.com/FTChinese/ftacademy/pkg/price"
+	"github.com/FTChinese/ftacademy/pkg/sq"
+	"github.com/FTChinese/go-rest/chrono"
+	"github.com/FTChinese/go-rest/enum"
+)
+
+type LicenceQueue struct {
+	ID           string         `json:"id" db:"id"`
+	FinalizedUTC chrono.Time    `json:"finalizedUtc" db:"finalized_utc"`
+	Index        int64          `json:"-" db:"order_by_index"`
+	Kind         enum.OrderKind `json:"kind" db:"kind"`                  // Only create or renew.
+	LicencePrior LicenceJSON    `json:"licencePrior" db:"licence_prior"` // The licence when this row is created. Do not use it to build renewed licence since it might already become obsolete.
+	LicenceAfter LicenceJSON    `json:"licenceAfter" db:"licence_after"`
+	OrderID      string         `json:"orderId" db:"order_id"`
+	PriceID      string         `json:"priceId" db:"price_id"`
+}
+
+// NewLicenceQueue creates a new LicenceQueue for each
+// copy newly created or renewed.
+func NewLicenceQueue(orderID string, p price.Price, currLic licence.ExpandedLicence, i int) LicenceQueue {
+	var k enum.OrderKind
+	if currLic.ID == "" {
+		k = enum.OrderKindCreate
+	} else {
+		k = enum.OrderKindRenew
+	}
+
+	return LicenceQueue{
+		ID:           pkg.LicenceQueueItemID(),
+		FinalizedUTC: chrono.Time{},
+		Index:        int64(i),
+		Kind:         k,
+		LicencePrior: LicenceJSON{currLic},
+		LicenceAfter: LicenceJSON{},
+		OrderID:      orderID,
+		PriceID:      p.ID,
+	}
+}
+
+// Finalize after a licence is created or renewed.
+func (q LicenceQueue) Finalize(lic licence.ExpandedLicence) LicenceQueue {
+	q.FinalizedUTC = chrono.TimeNow()
+	q.LicenceAfter = LicenceJSON{lic}
+
+	return q
+}
+
+// RowValues build the values of an SQL bulk insert.
+func (q LicenceQueue) RowValues() []interface{} {
+	return []interface{}{
+		q.ID,
+		q.FinalizedUTC,
+		q.Index,
+		q.Kind,
+		q.LicencePrior,
+		q.LicenceAfter,
+		q.OrderID,
+		q.PriceID,
+	}
+}
+
+// BulkLicenceQueue is used to build the values part of
+// bulk insert, together with StmtBulkLicenceQueue.
+type BulkLicenceQueue []LicenceQueue
+
+func (b BulkLicenceQueue) Each(handler func(row sq.BulkInsertRow)) {
+	for _, v := range b {
+		handler(v)
+	}
+}
+
+// GroupedQueues groups LicenceQueue for the specified
+// price group of an order.
+type GroupedQueues struct {
+	PriceID  string         `json:"priceId"`
+	Creation []LicenceQueue `json:"creation"`
+	Renewal  []LicenceQueue `json:"renewal"`
+}
+
+func NewGroupedQueues(priceID string, rows []LicenceQueue) GroupedQueues {
+	var g = GroupedQueues{
+		PriceID: priceID,
+	}
+
+	for _, item := range rows {
+		if item.PriceID != priceID {
+			continue
+		}
+
+		switch item.Kind {
+		case enum.OrderKindCreate:
+			g.Creation = append(g.Creation, item)
+		case enum.OrderKindRenew:
+			g.Renewal = append(g.Renewal, item)
+		}
+	}
+
+	return g
+}
+
+// PriceOfQueue maps the LicenceQueue id to the price it used.
+type PriceOfQueue struct {
+	QueueID string      `db:"queue_id"`
+	Price   price.Price `db:"price"`
+}
---
+++ internal/pkg/checkout/licence_queue_stmt.go
@@ -0,0 +1,69 @@
+package checkout
+
+import "github.com/FTChinese/ftacademy/pkg/sq"
+
+// StmtBulkLicenceQueue build the SQL to bulk insert into
+// licence_queue table.
+// Its value is built by BulkLicenceQueue.
+func StmtBulkLicenceQueue(n int) sq.BulkInsert {
+	return sq.NewBulkInsert().
+		Into(sq.NewTable("licence_queue")).
+		SetColumns(
+			sq.NewColumn("id"),
+			sq.NewColumn("finalized_utc"),
+			sq.NewColumn("order_by_index"),
+			sq.NewColumn("kind"),
+			sq.NewColumn("licence_prior"),
+			sq.NewColumn("licence_after"),
+			sq.NewColumn("order_id"),
+			sq.NewColumn("price_id"),
+		).
+		Rows(n)
+}
+
+const StmtFinalizeLicenceQueue = `
+UPDATE b2b.licence_queue
+SET finalized_utc = :finalized_utc,
+	licence_after = :licence_after
+WHERE id = :id
+LIMIT 1`
+
+const colLicenceQueue = `
+SELECT qid,
+	finalized_utc,
+	order_by_index,
+	kind,
+	licence_prior,
+	licence_after,
+	order_id,
+	price_id
+FROM b2b.licence_queue
+`
+
+// StmtListLicenceQueue retrieves all queued licence of
+// a specific price of an order.
+const StmtListLicenceQueue = colLicenceQueue + `
+WHERE order_id = ?
+	AND price_id = ?
+ORDER BY kind ASC, order_by_index ASC
+`
+
+// StmtLockLicenceQueue locks a row in licence queue
+// when payment is confirmed.
+const StmtLockLicenceQueue = colLicenceQueue + `
+WHERE id = ?
+LIMIT 1
+FOR UPDATE
+`
+
+// StmtListPriceOfQueue retrieve a licence queue and the price
+// to bulk process licence creation/renewal.
+const StmtListPriceOfQueue = `
+SELECT q.id AS queue_id,
+	c.price AS price
+FROM b2b.licence_queue AS q
+	LEFT JOIN cart_item AS c
+ON q.order_id = c.order_id
+	AND q.price_id = c.price_id	
+WHERE q.order_id = ?
+`
