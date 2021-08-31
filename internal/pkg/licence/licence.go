eed50c92d77565751e8ab3c04fe52a6dac81492b
weiguo.ni <neefrankie@gmail.com>
2021-08-31 12:44:48 +0800

    refactor: rename BaseLicence to Licence

--- internal/pkg/licence/licence.go
+++ internal/pkg/licence/licence.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-// BaseLicence represents a row in the licence table.
+// Licence represents a row in the licence table.
 // A team can purchase multiple licences, as they usually do.
 // After purchasing licences, the team can send
 // invitations to team member for a specific licence.
@@ -21,10 +21,10 @@ import (
 // When a licence is revoked, backup reader's current
 // membership status and delete that row, clear
 // the AssigneeID field.
-type BaseLicence struct {
+type Licence struct {
 	ID string `json:"id" db:"licence_id"`
 	price.Edition
-	Creator
+	admin.Creator
 	// The following fields are subject to change.
 	Status                Status         `json:"status" db:"lic_status"`
 	CurrentPeriodStartUTC chrono.Time    `json:"currentPeriodStartUtc" db:"current_period_start_utc"`
@@ -36,23 +36,21 @@ type BaseLicence struct {
 	LatestPrice           price.Price    `json:"latestPrice" db:"latest_price"`
 	LatestInvitation      InvitationJSON `json:"latestInvitation" db:"latest_invitation"` // A redundant field that should be synced with the invitation row. I created this field since I don't know how to retrieve both licence and invitation row in SQL's flat structure, specially used when retrieve a list of such rows.
 	AssigneeID            null.String    `json:"-" db:"assignee_id"`
-	RowTime
+	admin.RowTime
 }
 
-func NewBaseLicence(p price.Price, orderID string, creator admin.PassportClaims) BaseLicence {
+func NewLicence(p price.Price, orderID string, creator admin.Creator) Licence {
 
+	// TODO: pass as parameter
 	now := time.Now()
 
 	period := dt.NewTimeRange(now).
 		WithCycle(p.Cycle)
 
-	return BaseLicence{
-		ID:      pkg.LicenceID(),
-		Edition: p.Edition,
-		Creator: Creator{
-			CreatorID: creator.AdminID,
-			TeamID:    creator.TeamID.String,
-		},
+	return Licence{
+		ID:                    pkg.LicenceID(),
+		Edition:               p.Edition,
+		Creator:               creator,
 		CurrentPeriodStartUTC: chrono.TimeUTCFrom(period.Start),
 		CurrentPeriodEndUTC:   chrono.TimeUTCFrom(period.End),
 		StartDateUTC:          chrono.TimeUTCFrom(now),
@@ -63,8 +61,18 @@ func NewBaseLicence(p price.Price, orderID string, creator admin.PassportClaims)
 		Status:                LicStatusAvailable,
 		LatestInvitation:      InvitationJSON{},
 		AssigneeID:            null.String{},
-		RowTime:               NewRowTime(),
+		RowTime:               admin.NewRowTime(),
 	}
+}
+
+func (l Licence) RenewalStartTime() time.Time {
+	now := time.Now()
+
+	if l.CurrentPeriodEndUTC.After(now) {
+		return l.CurrentPeriodEndUTC.Time
+	}
+
+	return now
 }
 
 // IsAvailable checks whether the licence is available to
@@ -75,12 +83,12 @@ func NewBaseLicence(p price.Price, orderID string, creator admin.PassportClaims)
 // generate multiple invitations.
 // However, only the latest invitation could be accepted.
 // All previous invitations will be invalidated in such case.
-func (l BaseLicence) IsAvailable() bool {
+func (l Licence) IsAvailable() bool {
 	return l.Status != LicStatusGranted && l.AssigneeID.IsZero()
 }
 
 // WithInvitation syncs a licence's latest_invitation when a new invitation is create for it.
-func (l BaseLicence) WithInvitation(inv Invitation) BaseLicence {
+func (l Licence) WithInvitation(inv Invitation) Licence {
 	l.Status = LicStatusInvited
 	l.LatestInvitation = InvitationJSON{inv}
 	l.AssigneeID = null.String{}
@@ -92,7 +100,7 @@ func (l BaseLicence) WithInvitation(inv Invitation) BaseLicence {
 // have its invitation revoked.
 // A licence could only have its invitation revoked when
 // an invitation is sent but not accepted.
-func (l BaseLicence) IsInvitationRevocable() bool {
+func (l Licence) IsInvitationRevocable() bool {
 	return l.Status == LicStatusInvited && l.AssigneeID.IsZero()
 }
 
@@ -101,7 +109,7 @@ func (l BaseLicence) IsInvitationRevocable() bool {
 // so that admin could invite another one to use this licence.
 // This is used after invitation is sent but before it is
 // accepted.
-func (l BaseLicence) WithInvitationRevoked() BaseLicence {
+func (l Licence) WithInvitationRevoked() Licence {
 	l.Status = LicStatusAvailable
 	l.LatestInvitation = InvitationJSON{}
 	l.AssigneeID = null.String{}
@@ -111,7 +119,7 @@ func (l BaseLicence) WithInvitationRevoked() BaseLicence {
 }
 
 // Granted links a licence to a ftc reader.
-func (l BaseLicence) Granted(a Assignee, inv Invitation) BaseLicence {
+func (l Licence) Granted(a Assignee, inv Invitation) Licence {
 	l.Status = LicStatusGranted
 	l.LatestInvitation = InvitationJSON{inv}
 	l.AssigneeID = a.FtcID
@@ -120,22 +128,27 @@ func (l BaseLicence) Granted(a Assignee, inv Invitation) BaseLicence {
 	return l
 }
 
+// IsGranted tests whether a licence is granted to any reader.
+func (l Licence) IsGranted() bool {
+	return l.Status == LicStatusGranted && l.AssigneeID.Valid
+}
+
 // IsGrantedTo checks if a licence is granted to the
 // specified membership.
-func (l BaseLicence) IsGrantedTo(m reader.Membership) bool {
+func (l Licence) IsGrantedTo(m reader.Membership) bool {
 	if !m.IsB2B() {
 		return false
 	}
 
-	return m.B2BLicenceID.String == l.ID
+	return l.ID == m.B2BLicenceID.String
 }
 
-func (l BaseLicence) IsRevocable() bool {
+func (l Licence) IsRevocable() bool {
 	return l.Status == LicStatusGranted && l.AssigneeID.Valid
 }
 
 // Revoked unlink a user from a licence.
-func (l BaseLicence) Revoked() BaseLicence {
+func (l Licence) Revoked() Licence {
 	l.Status = LicStatusAvailable
 	l.LatestInvitation = InvitationJSON{}
 	l.AssigneeID = null.String{}
@@ -144,16 +157,31 @@ func (l BaseLicence) Revoked() BaseLicence {
 	return l
 }
 
-// Licence contains data of a licence row joined with user info.
+func (l Licence) Renewed(p price.Price, orderID string) Licence {
+	now := time.Now()
+
+	startTime := l.RenewalStartTime()
+	period := dt.NewTimeRange(startTime).WithCycle(p.Cycle)
+
+	l.CurrentPeriodStartUTC = chrono.TimeUTCFrom(period.Start)
+	l.CurrentPeriodEndUTC = chrono.TimeUTCFrom(period.End)
+	l.LatestPrice = p
+	l.LatestOrderID = null.StringFrom(orderID)
+	l.UpdatedUTC = chrono.TimeUTCFrom(now)
+
+	return l
+}
+
+// ExpandedLicence contains data of a licence row joined with user info.
 // This is used mostly when retrieving a list of licence.
-type Licence struct {
-	BaseLicence
+type ExpandedLicence struct {
+	Licence
 	// Only exists after reader accepted an invitation.
 	// Join with userinfo table
 	Assignee AssigneeJSON `json:"assignee" db:"assignee"`
 }
 
-type LicList struct {
+type PagedLicenceList struct {
 	pkg.PagedList
-	Data []Licence `json:"data"`
+	Data []ExpandedLicence `json:"data"`
 }
