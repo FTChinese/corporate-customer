9afbd781b209bf4b0c5aff85d023dcefbb863198
weiguo.ni <neefrankie@gmail.com>
2021-08-31 11:31:16 +0800

    feat: methods for where clause

--- pkg/sq/where.go
+++ pkg/sq/where.go
@@ -2,3 +2,35 @@ package sq
 
 type Where struct {
 }
+
+func NewWhere(col Column) Where {
+	return Where{}
+}
+
+func NewWhereParen(p Paren) Where {
+	return Where{}
+}
+
+func (w Where) AddColumn(col Column) Where {
+	return w
+}
+
+func (w Where) AddParen(p Paren) Where {
+	return w
+}
+
+func (w Where) And(col Column) Where {
+	return w
+}
+
+func (w Where) AndParen(p Paren) Where {
+	return w
+}
+
+func (w Where) Or(col Column) Where {
+	return w
+}
+
+func (w Where) OrParen(p Paren) Where {
+	return w
+}
