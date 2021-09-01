d3f80c43147d0394ff893309b9a618f316210adb
weiguo.ni <neefrankie@gmail.com>
2021-09-01 11:23:42 +0800

    test

--- internal/app/b2b/repository/txrepo/order_tx_test.go
+++ internal/app/b2b/repository/txrepo/order_tx_test.go
@@ -2,7 +2,9 @@ package txrepo
 
 import (
 	"github.com/FTChinese/ftacademy/internal/mock"
+	"github.com/FTChinese/ftacademy/internal/pkg"
 	"github.com/FTChinese/ftacademy/internal/pkg/checkout"
+	"github.com/FTChinese/ftacademy/internal/pkg/licence"
 	"github.com/FTChinese/ftacademy/pkg/db"
 	"github.com/FTChinese/ftacademy/pkg/faker"
 	"github.com/FTChinese/ftacademy/pkg/price"
@@ -58,6 +60,98 @@ func TestTxRepo_CreateOrder(t *testing.T) {
 
 			if err := tx.CreateOrder(tt.args.order); (err != nil) != tt.wantErr {
 				t.Errorf("CreateOrder() error = %v, wantErr %v", err, tt.wantErr)
+				_ = tx.Rollback()
+				return
+			}
+
+			_ = tx.Commit()
+		})
+	}
+}
+
+func TestTxRepo_SaveCartItem(t *testing.T) {
+	type fields struct {
+		Tx *sqlx.Tx
+	}
+	type args struct {
+		c checkout.CartItemSchema
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save car item",
+			fields: fields{
+				Tx: db.MockTx(),
+			},
+			args: args{
+				c: checkout.CartItem{
+					Price:     price.MockPriceStdYear,
+					NewCopies: 5,
+					Renewals: checkout.ExpLicenceListJSON{
+						licence.NewLicBuilder(price.MockPriceStdYear).Build(),
+						licence.NewLicBuilder(price.MockPriceStdYear).Build(),
+					},
+				}.Schema(pkg.OrderID()),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := TxRepo{
+				Tx: tt.fields.Tx,
+			}
+			if err := tx.SaveCartItem(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("SaveCartItem() error = %v, wantErr %v", err, tt.wantErr)
+				_ = tx.Rollback()
+				return
+			}
+
+			_ = tx.Commit()
+		})
+	}
+}
+
+func TestTxRepo_SaveLicenceQueue(t *testing.T) {
+	type fields struct {
+		Tx *sqlx.Tx
+	}
+	type args struct {
+		q checkout.BulkLicenceQueue
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Save licence queue",
+			fields: fields{
+				Tx: db.MockTx(),
+			},
+			args: args{
+				q: checkout.NewCartBuilder().
+					AddStandardN(5).
+					AddRenewal(licence.NewLicBuilder(price.MockPriceStdYear).Build()).
+					AddPremiumN(2).
+					Build().
+					LicenceQueue(pkg.OrderID()),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := TxRepo{
+				Tx: tt.fields.Tx,
+			}
+			if err := tx.SaveLicenceQueue(tt.args.q); (err != nil) != tt.wantErr {
+				t.Errorf("SaveLicenceQueue() error = %v, wantErr %v", err, tt.wantErr)
 				_ = tx.Rollback()
 				return
 			}
