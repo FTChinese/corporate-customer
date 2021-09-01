3c673091f3fca39bafcd1f42902a5686876478df
weiguo.ni <neefrankie@gmail.com>
2021-09-01 09:47:13 +0800

    refactor: removed mocking data

--- internal/pkg/admin/mock.go
+++ internal/pkg/admin/mock.go
@@ -3,57 +3,16 @@
 package admin
 
 import (
-	"github.com/FTChinese/ftacademy/internal/pkg/input"
-	"github.com/FTChinese/ftacademy/pkg/faker"
-	"github.com/FTChinese/go-rest/chrono"
-	"github.com/brianvoe/gofakeit/v5"
+	"github.com/FTChinese/ftacademy/internal/pkg"
 	"github.com/google/uuid"
 	"github.com/guregu/null"
+	"time"
 )
 
-func MockAccount() Account {
-	return Account{
-		BaseAccount: BaseAccount{
-			ID:          uuid.New().String(),
-			TeamID:      null.String{},
-			Email:       faker.GenEmail(),
-			DisplayName: null.String{},
-			Active:      true,
-			Verified:    false,
-		},
-		Password:   faker.SimplePassword(),
-		CreatedUTC: chrono.TimeNow(),
-		UpdatedUTC: chrono.Time{},
+func MockPassportClaims() PassportClaims {
+	return PassportClaims{
+		AdminID:        uuid.New().String(),
+		TeamID:         null.StringFrom(pkg.TeamID()),
+		StandardClaims: NewStandardClaims(time.Now().Unix() + 86400*7),
 	}
-}
-
-func MockEmailVerifier(email string) EmailVerifier {
-	v, err := NewEmailVerifier(email)
-	if err != nil {
-		panic(err)
-	}
-
-	return v
-}
-
-func MockPwResetSession(params input.ForgotPasswordParams) PwResetSession {
-	s, err := NewPwResetSession(params)
-	if err != nil {
-		panic(err)
-	}
-
-	return s
-}
-
-func MockTeamParams() input.TeamParams {
-	faker.SeedGoFake()
-
-	return input.TeamParams{
-		OrgName:      gofakeit.Company(),
-		InvoiceTitle: null.StringFrom(gofakeit.Company()),
-	}
-}
-
-func MockTeam() Team {
-	return NewTeam(uuid.New().String(), MockTeamParams())
 }
