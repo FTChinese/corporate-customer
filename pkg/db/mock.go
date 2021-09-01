c3938ab96671debff37f4daedec0f81225455261
weiguo.ni <neefrankie@gmail.com>
2021-09-01 09:42:59 +0800

    feat: MockTX

--- pkg/db/mock.go
+++ pkg/db/mock.go
@@ -4,9 +4,14 @@ package db
 
 import (
 	"github.com/FTChinese/ftacademy/pkg/config"
+	"github.com/jmoiron/sqlx"
 )
 
 func MockMySQL() ReadWriteMyDBs {
 	config.MustSetupViper(config.MustReadConfigFile())
 	return MustNewMyDBs(false)
+}
+
+func MockTx() *sqlx.Tx {
+	return MockMySQL().Write.MustBegin()
 }
