cc1a45665323bb82f5d33650eaab365dac29c316
weiguo.ni <neefrankie@gmail.com>
2021-09-01 09:44:55 +0800

    refactor: merge OrderInputSchema

--- internal/pkg/checkout/order.go
+++ internal/pkg/checkout/order.go
@@ -47,3 +47,21 @@ type OrderList struct {
 	pkg.PagedList
 	Data []Order `json:"data"`
 }
+
+// OrderInputSchema is db schema to save a shopping cart.
+// A shopping cart is dissected into 3 tables.
+type OrderInputSchema struct {
+	OrderRow  Order
+	CartItems []CartItemSchema
+	Queue     []LicenceQueue // All copies created from shopping cart items.
+}
+
+func NewOrderInputSchema(cart ShoppingCart, p admin.PassportClaims) OrderInputSchema {
+	o := NewOrder(cart, p)
+
+	return OrderInputSchema{
+		OrderRow:  o,
+		CartItems: cart.CartItemSchema(o.ID),
+		Queue:     cart.LicenceQueue(o.ID),
+	}
+}
