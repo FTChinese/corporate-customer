0600d3bf862f7e6d53fa14591caa3b47eeb9943f
weiguo.ni <neefrankie@gmail.com>
2021-09-18 18:43:41 +0800

    refactor: shopping cart to db order

--- internal/pkg/checkout/shopping_cart.go
+++ internal/pkg/checkout/shopping_cart.go
@@ -1,14 +1,18 @@
 package checkout
 
 import (
+	"github.com/FTChinese/ftacademy/internal/pkg"
+	"github.com/FTChinese/ftacademy/internal/pkg/admin"
 	"github.com/FTChinese/ftacademy/internal/pkg/licence"
+	"github.com/FTChinese/go-rest/chrono"
 )
 
 // ShoppingCart is used to hold data submitted by client.
 // When storing a shopping cart, its data is decomposed:
 // A summary of items is saved in an order row;
 // the items array is saved into a separate table one by one.
-// Table `order` to `order_item` has a one-many relation.
+// Each licence derived from Items will be saved into
+// licence_queue table
 type ShoppingCart struct {
 	Items       []CartItem `json:"items"`
 	ItemCount   int64      `json:"itemCount"`
@@ -26,41 +30,80 @@ func (c ShoppingCart) OrderItemList() OrderItemListJSON {
 	return list
 }
 
-// CartItemSchema turns Items to a list to be saved one
-// row per element in a table separate from order.
-func (c ShoppingCart) CartItemSchema(orderID string) []CartItemSchema {
+type OrderSchemaBuilder struct {
+	orderID string
+	cart    ShoppingCart
+	creator admin.Creator
+}
+
+func NewOrderSchemaBuilder(cart ShoppingCart, pp admin.PassportClaims) OrderSchemaBuilder {
+	return OrderSchemaBuilder{
+		orderID: pkg.OrderID(),
+		cart:    cart,
+		creator: admin.Creator{
+			AdminID: pp.AdminID,
+			TeamID:  pp.TeamID.String,
+		},
+	}
+}
+
+func (b OrderSchemaBuilder) Order() Order {
+	return Order{
+		ID:            b.orderID,
+		Creator:       b.creator,
+		AmountPayable: b.cart.TotalAmount,
+		CreatedUTC:    chrono.TimeUTCNow(),
+		ItemList:      b.cart.OrderItemList(),
+		ItemCount:     b.cart.ItemCount,
+		Status:        StatusPending,
+	}
+}
+
+func (b OrderSchemaBuilder) CartItemSchema() []CartItemSchema {
 	var s = make([]CartItemSchema, 0)
 
-	for _, item := range c.Items {
-		s = append(s, item.Schema(orderID))
+	for _, item := range b.cart.Items {
+		s = append(s, item.Schema(b.orderID, b.creator))
 	}
 
 	return s
 }
 
-// LicenceQueue create db schema to queue the licence-to-be-created.
-func (c ShoppingCart) LicenceQueue(orderID string) []LicenceQueue {
-	var queue = make([]LicenceQueue, 0)
+func (b OrderSchemaBuilder) TransactionList() []LicenceTransaction {
+	var txnList = make([]LicenceTransaction, 0)
 
 	// Loop over each cart item
-	index := 0
-	for _, item := range c.Items {
+	for _, item := range b.cart.Items {
 		// Loop over each new copy of a cart item.
 		for i := 0; i < int(item.NewCopies); i++ {
-			index += 1
-
-			qi := NewLicenceQueue(orderID, item.Price, licence.ExpandedLicence{}, index)
-			queue = append(queue, qi)
+			txn := NewLicenceTransaction(
+				b.orderID,
+				item.Price,
+				b.creator,
+				licence.ExpandedLicence{},
+			)
+			txnList = append(txnList, txn)
 		}
 
 		// Loop over each renewed licence.
 		for _, lic := range item.Renewals {
-			index += 1
-
-			item := NewLicenceQueue(orderID, item.Price, lic, index)
-			queue = append(queue, item)
+			item := NewLicenceTransaction(
+				b.orderID,
+				item.Price,
+				b.creator,
+				lic,
+			)
+			txnList = append(txnList, item)
 		}
 	}
 
-	return queue
+	return txnList
+}
+
+func (b OrderSchemaBuilder) Build() OrderInputSchema {
+	return OrderInputSchema{
+		OrderRow:     b.Order(),
+		CartItems:    b.CartItemSchema(),
+		Transactions: b.TransactionList(),
+	}
 }
