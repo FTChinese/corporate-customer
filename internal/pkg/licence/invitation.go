4b74816fe64bbc16409cc1da3afdd906636fb10f
weiguo.ni <neefrankie@gmail.com>
2021-09-18 18:33:34 +0800

    feat: InvitationJSON

--- internal/pkg/licence/invitation.go
+++ internal/pkg/licence/invitation.go
@@ -1,9 +1,6 @@
 package licence
 
 import (
-	"database/sql/driver"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/FTChinese/ftacademy/internal/pkg"
 	"github.com/FTChinese/ftacademy/internal/pkg/admin"
@@ -97,53 +94,6 @@ func (i Invitation) Revoked() Invitation {
 
 func (i Invitation) FormatDuration() string {
 	return fmt.Sprintf("%d天", i.ExpirationDays)
-}
-
-// InvitationJSON is used to implement sql Valuer interface.
-// Problems if you implement it on Invitation: when used
-// as a field, the sql driver could save/retrieve a column
-// as JSON; however, when you want to use Invitation as a
-// plain SQL row, it continues to you custom `scan`, expecting
-// JSON value instead of plain SQL columns.
-type InvitationJSON struct {
-	Invitation
-}
-
-// Value implements Valuer interface by serializing an Invitation into
-// JSON data.
-func (i InvitationJSON) Value() (driver.Value, error) {
-	if i.ID == "" {
-		return nil, nil
-	}
-
-	b, err := json.Marshal(i)
-	if err != nil {
-		return nil, err
-	}
-
-	return string(b), nil
-}
-
-// Scan implements Valuer interface by deserializing an invitation field.
-func (i *InvitationJSON) Scan(src interface{}) error {
-	if src == nil {
-		*i = InvitationJSON{}
-		return nil
-	}
-
-	switch s := src.(type) {
-	case []byte:
-		var tmp InvitationJSON
-		err := json.Unmarshal(s, &tmp)
-		if err != nil {
-			return err
-		}
-		*i = tmp
-		return nil
-
-	default:
-		return errors.New("incompatible type to scan")
-	}
 }
 
 // InvitationList is used for restful output.
