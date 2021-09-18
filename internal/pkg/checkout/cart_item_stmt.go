ae8ec33e8b3ac7c4b1b1a8285b9d638dfa6a9690
weiguo.ni <neefrankie@gmail.com>
2021-09-18 18:37:44 +0800

    feat: rename table

--- internal/pkg/checkout/cart_item_stmt.go
+++ internal/pkg/checkout/cart_item_stmt.go
@@ -1,10 +1,12 @@
 package checkout
 
 const StmtInsertCartItem = `
-INSERT INTO b2b.cart_item
+INSERT INTO b2b.order_item
 SET order_id = :order_id,
 	price = :price,
 	new_copy_count = :new_copy_count,
 	renewal_list = :renewal_list,
+	admin_id = :admin_id,
+	team_id = :team_id,
 	created_utc = :created_utc
 `
