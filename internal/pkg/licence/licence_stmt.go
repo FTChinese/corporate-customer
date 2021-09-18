b69bcab89f795d3b17484c639a47ceac26d769fd
weiguo.ni <neefrankie@gmail.com>
2021-09-18 18:35:05 +0800

    feat: add hint_grant_mismatch and latest_transaction_id columns

--- internal/pkg/licence/licence_stmt.go
+++ internal/pkg/licence/licence_stmt.go
@@ -12,7 +12,8 @@ SELECT l.id AS licence_id,
 	l.start_date_utc AS start_date_utc,
 	l.trial_start_utc AS trial_start_utc,
 	l.trial_end_utc AS trial_end_utc,
-	l.latest_order_id AS latest_order_id,
+	l.hint_grant_mismatch,
+	l.latest_transaction_id AS latest_transaction_id,
 	l.latest_price AS latest_price,
 	l.latest_invitation AS latest_invitation,
 	l.assignee_id AS assignee_id,
@@ -42,62 +43,32 @@ SELECT COUNT(*) AS total_licence
 FROM b2b.licence
 WHERE team_id = ?`
 
-// StmtLockBaseLicence locks a row from licence table when
+// StmtLockLicence locks a row from licence table when
 // admin updates it.
-const StmtLockBaseLicence = colLicence + `
+const StmtLockLicence = colLicence + `
 FROM b2b.licence AS l
 WHERE l.id = ? AND l.team_id = ?
 LIMIT 1
 FOR UPDATE
 `
 
-// StmtLockBaseLicenceCMS locks a row in licence when
+// StmtLockLicenceCMS locks a row in licence when
 // updating it from CMS
-const StmtLockBaseLicenceCMS = colLicence + `
+const StmtLockLicenceCMS = colLicence + `
 FROM b2b.licence AS l
 WHERE l.id = ?
 LIMIT 1
 FOR UPDATE
 `
 
+// StmtUpdateLicenceStatus after a licence is granted or revoked.
 const StmtUpdateLicenceStatus = `
 UPDATE b2b.licence
 SET current_status = :lic_status,
+	hint_grant_mismatch = :hint_grant_mismatch,
 	latest_invitation = :latest_invitation,
 	assignee_id = :assignee_id,
 	updated_utc = :updated_utc
 WHERE id = :licence_id 
 	AND team_id = :team_id
-LIMIT 1`
-
-const colLicenceUpsert = `
-current_period_start_utc = :current_period_start_utc,
-current_period_end_utc = :current_period_end_utc,
-latest_order_id = :latest_order_id,
-latest_price = :latest_price,
-updated_utc = :updated_utc`
-
-const StmtCreateLicence = `
-INSERT INTO b2b.licence
-SET id = :licence_id,
-	tier = :tier,
-	cycle = :cycle,
-	current_status = :lic_status,
-	creator_id = :creator_id,
-	team_id = :team_id,
-
-	start_date_utc = :start_date_utc,
-	trial_start_utc = :trial_start_utc,
-	trial_end_utc = :trial_end_utc,
-	
-	latest_invitation = :latest_invitation,
-	assignee_id = :assignee_id,
-	created_utc = :created_utc,
-` + colLicenceUpsert
-
-const StmtRenewLicence = `
-UPDATE b2b.licence
-SET 
-` + colLicenceUpsert + `
-WHERE licence_id = :licence_id
 LIMIT 1`
