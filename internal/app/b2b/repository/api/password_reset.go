c371cae84b5fba3a125cb528b4f77ace91c92bfd
weiguo.ni <neefrankie@gmail.com>
2021-10-26 18:20:59 +0800

    feat: password reset

---
+++ internal/app/b2b/repository/api/password_reset.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"github.com/FTChinese/ftacademy/pkg/fetch"
+	"io"
+	"net/http"
+)
+
+func (c Client) ResetPassword(body io.Reader) (*http.Response, error) {
+	url := c.baseURL + pathBasePwReset
+
+	resp, errs := fetch.New().
+		Post(url).
+		SetBearerAuth(c.key).
+		StreamJSON(body).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+func (c Client) RequestPasswordResetLetter(body io.Reader) (*http.Response, error) {
+	url := c.baseURL + pathPwResetRequestLetter
+
+	resp, errs := fetch.New().
+		Post(url).
+		SetBearerAuth(c.key).
+		StreamJSON(body).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
+
+func (c Client) VerifyResetToken(token string) (*http.Response, error) {
+	url := c.baseURL + pathPwResetVerifyToken + token
+
+	resp, errs := fetch.New().
+		Get(url).
+		SetBearerAuth(c.key).
+		End()
+
+	if errs != nil {
+		return nil, errs[0]
+	}
+
+	return resp, nil
+}
