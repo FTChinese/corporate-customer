6dfea73a0e0028590e64d5543cb4b2a25df2c286
weiguo.ni <neefrankie@gmail.com>
2021-11-06 14:39:16 +0800

    feat: wx auth handler

--- internal/app/b2b/controller/reader_auth_wx.go
+++ internal/app/b2b/controller/reader_auth_wx.go
@@ -1,16 +1,52 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/FTChinese/ftacademy/internal/pkg/reader"
+	"github.com/FTChinese/ftacademy/pkg/fetch"
+	"github.com/FTChinese/go-rest/render"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
 
 func (router ReaderRouter) WxRequestCode(c echo.Context) error {
+	sess, err := reader.NewWxOAuthSession(router.wxApp.AppID)
+	if err != nil {
+		return render.NewInternalError(err.Error())
+	}
 
-	return nil
+	return c.JSON(http.StatusOK, sess)
 }
 
 func (router ReaderRouter) WxLogin(c echo.Context) error {
-	return nil
+	defer c.Request().Body.Close()
+
+	header := router.collectClientHeader(c)
+
+	resp, err := router.apiClient.WxLogin(
+		router.wxApp.AppID,
+		c.Request().Body,
+		header)
+
+	if err != nil {
+		render.NewInternalError(err.Error())
+	}
+
+	return c.Stream(resp.StatusCode, fetch.ContentJSON, resp.Body)
 }
 
 func (router ReaderRouter) WxRefresh(c echo.Context) error {
-	return nil
+	defer c.Request().Body.Close()
+
+	header := router.collectClientHeader(c)
+
+	resp, err := router.apiClient.WxRefresh(
+		router.wxApp.AppID,
+		c.Request().Body,
+		header)
+
+	if err != nil {
+		render.NewInternalError(err.Error())
+	}
+
+	return c.Stream(resp.StatusCode, fetch.ContentJSON, resp.Body)
 }
