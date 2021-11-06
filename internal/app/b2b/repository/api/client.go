c5a6c6bc456660ba394d6ff939c65fbc4ed50970
weiguo.ni <neefrankie@gmail.com>
2021-11-06 23:46:28 +0800

    refactor: put wx oauth code url generation to under client

--- internal/app/b2b/repository/api/client.go
+++ internal/app/b2b/repository/api/client.go
@@ -21,6 +21,7 @@ const (
 	pathPwResetVerifyToken   = basePathPwReset + "/tokens/"
 	pathWxLogin              = basePathWxAuth + "/login"
 	pathWxRefresh            = basePathWxAuth + "/refresh"
+	pathWxCallback           = basePathWxAuth + "/callback/fta-reader"
 )
 
 const (
@@ -50,14 +51,35 @@ const (
 )
 
 type Client struct {
-	key     string
-	baseURL string
+	key                string
+	baseURL            string // Localhost for dev; otherwise online production.
+	wxRedirectBaseURLs map[bool]string
+	isProd             bool
 }
 
 func NewSubsAPIClient(prod bool) Client {
 	log.Printf("Client for subscription api running in production: %t", prod)
 	return Client{
 		key:     config.MustSubsAPIKey().Pick(prod),        // Pick the correct api access token
-		baseURL: config.MustSubsAPIv3BaseURL().Pick(false), // Always use localhost.
+		baseURL: config.MustSubsAPIv3BaseURL().Pick(false), // Always use localhost since this app is on the same server as API.
+		wxRedirectBaseURLs: map[bool]string{
+			// When this app is in production mode, we want wechat to
+			// redirect to production api.
+			true: "https://www.ftacademy.cn/api/v3",
+			// When this app is in development mode, we want Wechat to
+			// redirect to sandbox api so that changes won't affect current
+			// user.
+			false: "https://www.ftacademy.cn/api/sandbox",
+		},
+		isProd: prod,
 	}
+}
+
+func (c Client) WxOAuthSession(appID string) (WxOAuthSession, error) {
+	midwayBaseURL := c.wxRedirectBaseURLs[c.isProd]
+	cbURL := midwayBaseURL + pathWxCallback
+
+	req := NewWxOAuthCodeRequest(appID, cbURL)
+
+	return NewWxOAuthSession(req)
 }
--- internal/app/b2b/repository/api/client_test.go
+++ internal/app/b2b/repository/api/client_test.go
@@ -2,40 +2,45 @@ package api
 
 import (
 	"github.com/FTChinese/ftacademy/pkg/faker"
-	"net/http"
 	"testing"
 )
 
-func TestClient_Paywall(t *testing.T) {
+func TestClient_WxOAuthSession(t *testing.T) {
 	faker.MustSetupViper()
 
+	type args struct {
+		appID string
+	}
 	tests := []struct {
 		name    string
-		client  Client
-		want    *http.Response
+		fields  Client
+		args    args
+		want    WxOAuthSession
 		wantErr bool
 	}{
 		{
-			name:    "Production paywall",
-			client:  NewSubsAPIClient(true),
+			name:   "Generate wx oauth session",
+			fields: NewSubsAPIClient(false),
+			args: args{
+				appID: "app123456",
+			},
+			want:    WxOAuthSession{},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
-			got, err := tt.client.Paywall()
+			c := tt.fields
+			got, err := c.WxOAuthSession(tt.args.appID)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Paywall() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("WxOAuthSession() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-
-			t.Logf("Stats=%d", got.StatusCode)
-			t.Logf("Body=%s", faker.MustReadBody(got.Body))
-
 			//if !reflect.DeepEqual(got, tt.want) {
-			//	t.Errorf("Paywall() got = %v, want %v", got, tt.want)
+			//    t.Errorf("WxOAuthSession() got = %v, want %v", got, tt.want)
 			//}
+
+			t.Logf("%s", faker.MustMarshalIndent(got))
 		})
 	}
 }
