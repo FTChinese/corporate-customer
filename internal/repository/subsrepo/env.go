6fd40c5707a62e02d35ce6707d5a7dee61200373
weiguo.ni <neefrankie@gmail.com>
2022-02-14 14:10:09 +0800

    refactor: relocate folder

--- internal/repository/subsrepo/env.go
+++ internal/repository/subsrepo/env.go
@@ -1,8 +1,8 @@
 package subsrepo
 
 import (
-	"github.com/FTChinese/ftacademy/internal/app/repository"
-	"github.com/FTChinese/ftacademy/internal/app/repository/txrepo"
+	"github.com/FTChinese/ftacademy/internal/repository"
+	"github.com/FTChinese/ftacademy/internal/repository/txrepo"
 	"github.com/FTChinese/ftacademy/pkg/db"
 	"go.uber.org/zap"
 )
