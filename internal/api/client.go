c73fae75d46c835ee6bdf5443641d2a20dd3501d
weiguo.ni <neefrankie@gmail.com>
2022-06-20 12:49:03 +0800

    refactor: doc

--- internal/api/client.go
+++ internal/api/client.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"github.com/FTChinese/ftacademy/pkg/config"
-	"log"
 )
 
 type Client struct {
@@ -12,60 +11,52 @@ type Client struct {
 	isProd             bool
 }
 
-func NewSubsAPIClient(prod bool) Client {
-	log.Printf("Client for subscription api running in production: %t", prod)
-
-	baseURLs := config.MustSubsAPIv6BaseURL()
-	return Client{
-		key:     config.MustSubsAPIKey().Pick(prod), // Pick the correct api access token
-		baseURL: baseURLs.Pick(false),               // Always use localhost since this app is on the same server as API.
-		wxRedirectBaseURLs: map[bool]string{
-			// When this app is in production mode, we want wechat to
-			// redirect to production api.
-			true: baseURLs.Pick(true),
-			// When this app is in development mode, we want Wechat to
-			// redirect to sandbox api so that changes won't affect current
-			// user.
-			false: config.MustAPISandboxURL().Pick(true),
-		},
-		isProd: prod,
-	}
-}
-
 type Clients struct {
-	Test Client
-	Live Client
+	Sandbox Client // For test accounts
+	Live    Client // For non-test accounts
 }
 
+// NewClients creates both sandbox and live clients. Then are selected at runtime.
+// There are 4 combinations of live + production:
+//                 Live
+//            /           \
+//        True            False
+//      /      \       /        \
+// Production  Dev   Sandbox    Dev
+//
+// Usually the Dev points to localhost so that in development you are not bothered with
+// which account you are using.
+// Among them, production/development is determined upon app launch while live/sandbox
+// is determined at runtime.
 func NewClients(prod bool) Clients {
 	token := config.MustSubsAPIKey().Pick(prod)
 	// There are 3 urls to select:
 	// - Development always uses localhost
 	// - Production has a live mode and test mode, determined by current user account.
-	testURLSelector := config.MustAPISandboxURL()
-	liveURLSelector := config.MustSubsAPIv6BaseURL()
+	sandboxBaseURLs := config.MustAPISandboxURL()
+	liveBaseURLs := config.MustSubsAPIv6BaseURL()
 
 	// To relay wechat OAuth, localhost should never be used.
 	wxRelayBaseURLs := map[bool]string{
 		// When this app is in production mode, we want wechat to
 		// redirect to production api.
-		true: liveURLSelector.Pick(true),
+		true: liveBaseURLs.Pick(true),
 		// When this app is in development mode, we want Wechat to
 		// redirect to sandbox api so that changes won't affect current
 		// user.
-		false: testURLSelector.Pick(true),
+		false: sandboxBaseURLs.Pick(true),
 	}
 
 	return Clients{
-		Test: Client{
+		Sandbox: Client{
 			key:                token,
-			baseURL:            config.MustAPISandboxURL().Pick(prod), // Pick localhost or production test
+			baseURL:            sandboxBaseURLs.Pick(prod), // Pick localhost or production test
 			wxRedirectBaseURLs: wxRelayBaseURLs,
 			isProd:             prod,
 		},
 		Live: Client{
 			key:                token,
-			baseURL:            config.MustSubsAPIv6BaseURL().Pick(prod), // Pick localhost or production live.
+			baseURL:            liveBaseURLs.Pick(prod), // Pick localhost or production live.
 			wxRedirectBaseURLs: wxRelayBaseURLs,
 			isProd:             prod,
 		},
@@ -77,7 +68,7 @@ func (c Clients) Select(live bool) Client {
 		return c.Live
 	}
 
-	return c.Test
+	return c.Sandbox
 }
 
 func (c Client) WxOAuthSession(appID string) (WxOAuthCodeSession, error) {
